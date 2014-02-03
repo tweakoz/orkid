6d503d84a920920b2ab4a2085c6decbf1e17c093
TweakoZ <michael@tweakoz.com>
2014-02-03 14:12:09 -0800

    ..

--- ork.core/src/dataflow/graph.cpp
+++ ork.core/src/dataflow/graph.cpp
@@ -28,7 +28,12 @@ namespace ork { namespace dataflow {
 void graph_data::Describe()
 {
 	ork::reflect::RegisterMapProperty("Modules", &graph_data::mModules);
+	ork::reflect::RegisterProperty( "zzz_connections", & graph_inst::SerializeConnections, & graph_inst::DeserializeConnections );
+
+	ork::reflect::AnnotatePropertyForEditor< graph_data >("zzz_connections", "editor.visible", "false" );
+	ork::reflect::AnnotateClassForEditor< graph_data >("editor.object.ops", ConstString("dfgraph:dflowgraphedit import:dflowgraphimport export:dflowgraphexport") );
 	ork::reflect::AnnotatePropertyForEditor< graph_data >("Modules", "editor.factorylistbase", "dflow/dgmodule" );
+
 }
 graph_data::graph_data()
 	: mbTopologyIsDirty( true )
@@ -105,15 +110,108 @@ void graph_data::UnLockTopoSortedChildren() const
 {	mChildrenTopoSorted.UnLock();
 }
 ///////////////////////////////////////////////////////////////////////////////
+bool graph_data::SerializeConnections(ork::reflect::ISerializer &ser) const
+{	for( orklut<ork::PoolString,ork::Object*>::const_iterator it=mModules.begin(); it!=mModules.end(); it++ )
+	{	ork::Object* pobj = it->second;
+		ork::dataflow::dgmodule* pdgmodule = ork::rtti::autocast(pobj);
+		if( pdgmodule )
+		{	pdgmodule->SetName( it->first );
+		}
+	}
+	/////////////////////////////////////////////
+	int inumlinks = 0;
+	for( orklut<ork::PoolString,ork::Object*>::const_iterator it = mModules.begin(); it != mModules.end(); it++ )
+	{	dgmodule* pmodule = rtti::autocast( it->second );
+		if( pmodule )
+		{	int inuminputplugs = pmodule->GetNumInputs();
+			for( int ip=0; ip<inuminputplugs; ip++ )
+			{	ork::dataflow::inplugbase* pinput = pmodule->GetInput(ip);
+				const ork::dataflow::outplugbase* poutput = pinput->GetExternalOutput();
+				if( poutput )
+				{	module* poutmodule = rtti::autocast(poutput->GetModule());
+					inumlinks++;
+				}
+			}
+		}
+	}
+	ser.Serialize( inumlinks );
+	/////////////////////////////////////////////
+	for( orklut<ork::PoolString,ork::Object*>::const_iterator it = mModules.begin(); it != mModules.end(); it++ )
+	{	dgmodule* pmodule = rtti::autocast( it->second );
+		if( pmodule )
+		{	int inuminputplugs = pmodule->GetNumInputs();
+			for( int ip=0; ip<inuminputplugs; ip++ )
+			{	ork::dataflow::inplugbase* pinput = pmodule->GetInput(ip);
+				const ork::dataflow::outplugbase* poutput = pinput->GetExternalOutput();
+				if( poutput )
+				{	module* poutmodule = rtti::autocast(poutput->GetModule());
+					ser.Serialize(ork::PieceString(pmodule->GetName().c_str()));
+					ser.Serialize(ork::PieceString(pinput->GetName().c_str()));
+					ser.Serialize(ork::PieceString(poutput->GetModule()->GetName().c_str()));
+					ser.Serialize(ork::PieceString(poutput->GetName().c_str()));
+				}
+			}
+		}
+	}
+	/////////////////////////////////////////////
+	return true;
+}
+///////////////////////////////////////////////////////////////////////////////
+bool graph_data::DeserializeConnections(ork::reflect::IDeserializer &deser)
+{	for( orklut<ork::PoolString,ork::Object*>::const_iterator it=mModules.begin(); it!=mModules.end(); it++ )
+	{	ork::Object* pobj = it->second;
+		ork::dataflow::dgmodule* pdgmodule = ork::rtti::autocast(pobj);
+		if( pdgmodule )
+		{	pdgmodule->SetName( it->first );
+			pdgmodule->SetParent( this );
+		}
+	}
+	/////////////////////////////////////////////
+	// read number of links
+	int inumlinks = 0;
+	deser.Deserialize( inumlinks );
+	/////////////////////////////////////////////
+	for( int il=0; il<inumlinks; il++ )
+	{	ork::ResizableString inp_mod_name;
+		ork::ResizableString inp_plg_name;
+		ork::ResizableString out_mod_name;
+		ork::ResizableString out_plg_name;
+		deser.Deserialize( inp_mod_name );
+		deser.Deserialize( inp_plg_name );
+		deser.Deserialize( out_mod_name );
+		deser.Deserialize( out_plg_name );
+		/////////////////////////
+		// make the connection
+		/////////////////////////
+		orklut<ork::PoolString,ork::Object*>::const_iterator it_inp = mModules.find(ork::AddPooledString(inp_mod_name));
+		orklut<ork::PoolString,ork::Object*>::const_iterator it_out = mModules.find(ork::AddPooledString(out_mod_name));
+		if( it_inp!=mModules.end() && it_out!=mModules.end() )
+		{	dgmodule* pinp_mod = rtti::autocast( it_inp->second );
+			dgmodule* pout_mod = rtti::autocast( it_out->second );
+			ork::dataflow::inplugbase* inp_plug = rtti::autocast(pinp_mod->GetInputNamed( ork::AddPooledString(inp_plg_name) ));
+			ork::dataflow::outplugbase* out_plug = rtti::autocast(pout_mod->GetOutputNamed( ork::AddPooledString(out_plg_name) ));
+			if( inp_plug != 0 && out_plug != 0 )
+			{	inp_plug->SafeConnect( *this, out_plug );
+			}
+		}
+	}
+	/////////////////////////////////////////////
+	return true;
+}
+///////////////////////////////////////////////////////////////////////////////
+bool graph_data::PreDeserialize(reflect::IDeserializer &)
+{
+	LockTopoSortedChildrenForWrite(101);
+	Clear();
+	mModules.clear();
+	UnLockTopoSortedChildren();
+	return true;
+}///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 void graph_inst::Describe()
 {
-	ork::reflect::RegisterProperty( "zzz_connections", & graph_inst::SerializeConnections, & graph_inst::DeserializeConnections );
-	ork::reflect::AnnotatePropertyForEditor< graph_inst >("zzz_connections", "editor.visible", "false" );
-
-	ork::reflect::AnnotateClassForEditor< graph_inst >("editor.object.ops", ConstString("dfgraph:dflowgraphedit import:dflowgraphimport export:dflowgraphexport") );
 }
 ///////////////////////////////////////////////////////////////////////////////
 graph_inst::graph_inst()
@@ -169,13 +267,13 @@ bool graph_inst::DoNotify(const ork::event::Event *event)
 }
 ///////////////////////////////////////////////////////////////////////////////
 void graph_inst::Clear()
-{	mSourcePlugs.clear();
-	mSinkPlugs.clear();
-	while( false==mModuleQueue.empty() ) mModuleQueue.pop();
+{	while( false==mModuleQueue.empty() ) 
+		mModuleQueue.pop();
 	mbTopologyIsDirty = true;
 	mScheduler = 0;
 	mbAccumulateWork = false;
 	mbInProgress = false;
+	mExternal = 0;
 }
 ///////////////////////////////////////////////////////////////////////////////
 void graph_inst::ReInit()
@@ -262,20 +360,6 @@ void graph_inst::RefreshTopology(dgcontext& ctx )
 	mMutex.UnLock();
 }
 ///////////////////////////////////////////////////////////////////////////////
-void graph_inst::AddSourcePlug(inplugbase* pin)
-{	mMutex.Lock();
-	{	mSourcePlugs.push_back(pin);
-	}
-	mMutex.UnLock();
-}
-///////////////////////////////////////////////////////////////////////////////
-void graph_inst::AddSinkPlug(outplugbase* pout)
-{	mMutex.Lock();
-	{	mSinkPlugs.push_back(pout);
-	}
-	mMutex.UnLock();
-}
-///////////////////////////////////////////////////////////////////////////////
 bool graph_inst::IsDirty(void) const
 {	OrkAssert( mbTopologyIsDirty == false );
 	bool bdirty = false;
@@ -319,106 +403,6 @@ bool graph_inst::IsComplete() const
 	//	mScheduler->QueueModule( pmod );
 	//}
 }*/
-///////////////////////////////////////////////////////////////////////////////
-bool graph_inst::SerializeConnections(ork::reflect::ISerializer &ser) const
-{	for( orklut<ork::PoolString,ork::Object*>::const_iterator it=mModules.begin(); it!=mModules.end(); it++ )
-	{	ork::Object* pobj = it->second;
-		ork::dataflow::dgmodule* pdgmodule = ork::rtti::autocast(pobj);
-		if( pdgmodule )
-		{	pdgmodule->SetName( it->first );
-		}
-	}
-	/////////////////////////////////////////////
-	int inumlinks = 0;
-	for( orklut<ork::PoolString,ork::Object*>::const_iterator it = mModules.begin(); it != mModules.end(); it++ )
-	{	dgmodule* pmodule = rtti::autocast( it->second );
-		if( pmodule )
-		{	int inuminputplugs = pmodule->GetNumInputs();
-			for( int ip=0; ip<inuminputplugs; ip++ )
-			{	ork::dataflow::inplugbase* pinput = pmodule->GetInput(ip);
-				const ork::dataflow::outplugbase* poutput = pinput->GetExternalOutput();
-				if( poutput )
-				{	module* poutmodule = rtti::autocast(poutput->GetModule());
-					inumlinks++;
-				}
-			}
-		}
-	}
-	ser.Serialize( inumlinks );
-	/////////////////////////////////////////////
-	for( orklut<ork::PoolString,ork::Object*>::const_iterator it = mModules.begin(); it != mModules.end(); it++ )
-	{	dgmodule* pmodule = rtti::autocast( it->second );
-		if( pmodule )
-		{	int inuminputplugs = pmodule->GetNumInputs();
-			for( int ip=0; ip<inuminputplugs; ip++ )
-			{	ork::dataflow::inplugbase* pinput = pmodule->GetInput(ip);
-				const ork::dataflow::outplugbase* poutput = pinput->GetExternalOutput();
-				if( poutput )
-				{	module* poutmodule = rtti::autocast(poutput->GetModule());
-					ser.Serialize(ork::PieceString(pmodule->GetName().c_str()));
-					ser.Serialize(ork::PieceString(pinput->GetName().c_str()));
-					ser.Serialize(ork::PieceString(poutput->GetModule()->GetName().c_str()));
-					ser.Serialize(ork::PieceString(poutput->GetName().c_str()));
-				}
-			}
-		}
-	}
-	/////////////////////////////////////////////
-	return true;
-}
-///////////////////////////////////////////////////////////////////////////////
-bool graph_inst::DeserializeConnections(ork::reflect::IDeserializer &deser)
-{	for( orklut<ork::PoolString,ork::Object*>::const_iterator it=mModules.begin(); it!=mModules.end(); it++ )
-	{	ork::Object* pobj = it->second;
-		ork::dataflow::dgmodule* pdgmodule = ork::rtti::autocast(pobj);
-		if( pdgmodule )
-		{	pdgmodule->SetName( it->first );
-			pdgmodule->SetParent( this );
-		}
-	}
-	/////////////////////////////////////////////
-	// read number of links
-	int inumlinks = 0;
-	deser.Deserialize( inumlinks );
-	/////////////////////////////////////////////
-	for( int il=0; il<inumlinks; il++ )
-	{	ork::ResizableString inp_mod_name;
-		ork::ResizableString inp_plg_name;
-		ork::ResizableString out_mod_name;
-		ork::ResizableString out_plg_name;
-		deser.Deserialize( inp_mod_name );
-		deser.Deserialize( inp_plg_name );
-		deser.Deserialize( out_mod_name );
-		deser.Deserialize( out_plg_name );
-		/////////////////////////
-		// make the connection
-		/////////////////////////
-		orklut<ork::PoolString,ork::Object*>::const_iterator it_inp = mModules.find(ork::AddPooledString(inp_mod_name));
-		orklut<ork::PoolString,ork::Object*>::const_iterator it_out = mModules.find(ork::AddPooledString(out_mod_name));
-		if( it_inp!=mModules.end() && it_out!=mModules.end() )
-		{	dgmodule* pinp_mod = rtti::autocast( it_inp->second );
-			dgmodule* pout_mod = rtti::autocast( it_out->second );
-			ork::dataflow::inplugbase* inp_plug = rtti::autocast(pinp_mod->GetInputNamed( ork::AddPooledString(inp_plg_name) ));
-			ork::dataflow::outplugbase* out_plug = rtti::autocast(pout_mod->GetOutputNamed( ork::AddPooledString(out_plg_name) ));
-			if( inp_plug != 0 && out_plug != 0 )
-			{	inp_plug->SafeConnect( *this, out_plug );
-			}
-		}
-	}
-	/////////////////////////////////////////////
-	return true;
-}
-///////////////////////////////////////////////////////////////////////////////
-bool graph_inst::PreDeserialize(reflect::IDeserializer &)
-{
-	LockTopoSortedChildrenForWrite(101);
-	Clear();
-	mModules.clear();
-	mExternal = 0;
-	UnLockTopoSortedChildren();
-	return true;
-}
-
 ///////////////////////////////////////////////////////////////////////////////
 } }
 ///////////////////////////////////////////////////////////////////////////////
