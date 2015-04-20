43b09c40f0d3050c73a2c8e1a89cfc6c3b5b7b32
TweakoZ <michael@tweakoz.com>
2015-04-19 23:04:33 -0700

    switch tl lua 5.1.5 / luabind

--- ork.data/src/scripts/scene.lua
+++ ork.data/src/scripts/scene.lua
@@ -4,6 +4,18 @@ function printf(...)
 	print(string.format(...))
 end
 
+for k,v in pairs(ork) do
+	printf( "ork.%s: %s ", k,type(v) )
+end
+
+for k,v in pairs(ork.scene) do
+	printf( "ork.scene.%s: %s ", k,type(v) )
+end
+
+local s = ork.scene();
+
 printf("Hello world, from %s yo.\n",_VERSION)
-printf( "NumEnt: %d",scene.NumEntities() )
+printf( "NumEnt: %d",s.NumEntities() )
+--printf( "Ents: %s",s:GetEntities() )
+
 printf( "orksys: %s yo",orksys )
