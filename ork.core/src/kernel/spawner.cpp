9729d7bdf174c4d91ca94e501694847a6936c5b4
Michael T. Mayers <michael@tweakoz.com>
2019-08-23 15:20:46 -0700

    add spawner from vrx re enable lit textured shader route tga::dds filter to nvcompress external process invocation

---
+++ ork.core/src/kernel/spawner.cpp
@@ -0,0 +1,216 @@
+///////////////////////////////////////////////////////////////////////////////
+// MicroOrk (Orkid)
+// Copyright 1996-2013, Michael T. Mayers
+// Provided under the MIT License (see LICENSE.txt)
+///////////////////////////////////////////////////////////////////////////////
+
+#include <ork/kernel/spawner.h>
+#include <ork/kernel/string/string.h>
+#include <ork/file/path.h>
+#include <assert.h>
+#include <time.h>
+#include <signal.h>
+#include <errno.h>
+#include <vector>
+#include <string.h>
+
+#if ! defined(WIN32)
+#include <sys/wait.h>
+#include <unistd.h>
+#endif
+//#include <boost/algorithm/string.hpp>
+
+#if defined(__APPLE__)
+    extern char** environ;
+#endif
+
+namespace ork {
+///////////////////////////////////////////////////////////////////////////////
+// process spawn utils
+///////////////////////////////////////////////////////////////////////////////
+
+Spawner::Spawner()
+    : mExecRet(0)
+#if ! defined(WIN32)
+	, mChildPID(-1)
+#endif
+{
+
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+Spawner::~Spawner()
+{
+#if ! defined(WIN32)
+	if( mChildPID > 0 )
+    {
+        printf( "KILLING PID<%d>\n", mChildPID );
+        sendSignal(SIGKILL);
+    }
+#endif
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+void Spawner::sendSignal (int sig) {
+#if ! defined(WIN32)
+	kill(mChildPID, sig);
+#endif
+}
+
+void Spawner::spawnSynchronous(){
+  spawn();
+  collectZombie();
+}
+
+void Spawner::spawn()
+{
+#if ! defined(WIN32)
+	mChildPID = fork();
+
+    printf( "fork<%d>\n", mChildPID );
+    fflush(stdout);
+
+    if( 0 == mChildPID ) // child
+    {
+        /////////////////////////////////////////////////////////////
+        // build environ
+        /////////////////////////////////////////////////////////////
+
+        const Environment::env_map_t& emap = mEnvironment.RefMap();
+
+        size_t inum_vars = emap.size();
+
+        char** env_vars = (char**) malloc(sizeof(char*)*(inum_vars+1));
+
+        size_t icounter = 0;
+        for( const auto& item : emap )
+        {
+            const std::string& k = item.first;
+            const std::string& v = item.second;
+            std::string VAR = k + "=" + v;
+            env_vars[icounter] = strdup(VAR.c_str());
+            printf( "SETENV<%s>\n", env_vars[icounter] );
+            icounter++;
+        }
+        env_vars[icounter] = 0; // terminate envvar array
+
+        //printf( "child cp0 numenvvars<%d>\n", int(inum_vars) );
+        //fflush(stdout);
+
+        /////////////////////////////////////////////////////////////
+        // build args
+        /////////////////////////////////////////////////////////////
+
+        std::vector<std::string> vargs = SplitString(mCommandLine,' ');
+
+        //vargs.insert(vargs.begin(),vargs[0]);
+
+        size_t inum_args = vargs.size();
+
+        printf( "child cp1 numargs<%d>\n", int(inum_args) );
+        fflush(stdout);
+
+        char** args =  (char**) malloc(sizeof(char*)*(inum_args+1));
+
+        for( int i=0; i<inum_args; i++ )
+        {
+            const std::string& arg = vargs[i];
+
+            if( 0 == i )
+            {
+                auto argpath = file::Path(arg);
+                file::DecomposedPath dec;
+                argpath.DeCompose(dec);
+
+                auto exe = dec.mFile + dec.mExtension;
+
+                args[i] = strdup(exe.c_str());
+            }
+            else
+            {
+                args[i] = strdup(arg.c_str());
+            }
+
+            printf( "arg<%d> <%s>\n", i, args[i] );
+
+            //kernel::glog.printf( "spawn arg<%d:%s>\n", i, args[i] );
+        }
+        args[inum_args] = 0; // terminate arg array
+
+        //printf( "child cp2\n" );
+        //fflush(stdout);
+
+        /////////////////////////////////////////////////////////////
+        // set cwd
+        /////////////////////////////////////////////////////////////
+
+        if( mWorkingDirectory.length() )
+        {
+            printf( "child changing to directory<%s>\n", mWorkingDirectory.c_str() );
+            int iret = chdir( mWorkingDirectory.c_str() );
+            assert(iret==0);
+        }
+
+        /////////////////////////////////////////////////////////////
+        // exec
+        /////////////////////////////////////////////////////////////
+
+        printf( "child calling exec exe<%s>\n", args[0] );
+
+        #if defined(__APPLE__)
+        	::environ = env_vars;
+        	mExecRet = execvp(args[0], args);
+        #else
+            mExecRet = execvpe( args[0], args, env_vars );
+        #endif
+
+       // kernel::glog.printf( "fork failed <child> mExecRet<%d> ERRNO<%d>\n", mExecRet, errno );
+
+        perror("FORK FAILED");
+
+        assert(false); // if exec fails, exit forked process
+    }
+    else if( mChildPID<0 )
+    {
+        assert(false); // failed to fork
+    }
+    else // parent
+    {
+        //printf( "Spawned child pid<%d>\n", mChildPID );
+    }
+#endif
+}
+
+bool Spawner::is_alive()
+{
+#if defined(WIN32)
+	return false;
+#else
+	int status;
+    int err = waitpid(mChildPID, &status, WNOHANG);
+
+    if (-1 == err) {
+        printf("Spawner<%p>::is_alive: waitpid: %s\n", this, strerror(errno));
+        return false;
+    }
+
+    return 0 == err;
+#endif
+}
+
+/** Block until the child changes state. */
+void Spawner::collectZombie () {
+#if defined(WIN32)
+#else
+	int status;
+    int err = waitpid(mChildPID, &status, 0);
+
+    if (-1 == err) {
+        printf("Spawner<%p>::collectZombie: waitpid: %s\n", this, strerror(errno));
+    }
+#endif
+}
+///////////////////////////////////////////////////////////////////////////////
+} // namespace ork
--- ork.tool/src/filter/gfx/nvtt/nvtt_main.cpp
+++ ork.tool/src/filter/gfx/nvtt/nvtt_main.cpp
@@ -7,27 +7,10 @@
 
 #include <orktool/orktool_pch.h>
 #include <orktool/filter/filter.h>
+#include <ork/kernel/spawner.h>
 
 namespace ork { namespace tool {
 ///////////////////////////////////////////////////////////////////////////////
-
-#if 1
-bool NvttCompress( const ork::tool::FilterOptMap& options ){
-    assert(false);
-    return false;
-}
-
-#else
-
-#include "nvtt_main.h"
-
-/*
-The only precision issue (regarding S3TC on GC) has to do with the accuracy
-of the interpolation factors.  S3TC specifies 1/3 and 2/3.  GC uses 3/8 and 5/8.
-This gives a 4% error vs. the spec, or specifically:
-*/
-
-
 
 bool NvttCompress( const ork::tool::FilterOptMap& options ) ;
 
@@ -35,7 +18,7 @@ bool Tga2DdsFilterDriver( const tokenlist& toklist )
 {
 	ork::tool::FilterOptMap	OptionsMap;
 	OptionsMap.SetDefault( "-in", "yo.tga" );
-	OptionsMap.SetDefault( "-out", "yo.dds" );
+	OptionsMap.SetDefault( "-out", "" );
 	OptionsMap.SetDefault( "-mipfilter", "kaiser" );
 	OptionsMap.SetDefault( "-platform", "pc" );
 	OptionsMap.SetOptions( toklist );
@@ -45,207 +28,61 @@ bool Tga2DdsFilterDriver( const tokenlist& toklist )
 }
 
 
-static void setColorToNormalMap(nvtt::InputOptions & inputOptions)
-{
-	inputOptions.setNormalMap(false);
-	inputOptions.setConvertToNormalMap(true);
-	inputOptions.setHeightEvaluation(1.0f/3.0f, 1.0f/3.0f, 1.0f/3.0f, 0.0f);
-	//inputOptions.setNormalFilter(1.0f, 0, 0, 0);
-	//inputOptions.setNormalFilter(0.0f, 0, 0, 1);
-	inputOptions.setGamma(1.0f, 1.0f);
-	inputOptions.setNormalizeMipmaps(true);
-}
-
-///////////////////////////////////////////////////////////////////////////////
-// Set options for normal maps.
-///////////////////////////////////////////////////////////////////////////////
-static void setNormalMap(nvtt::InputOptions & inputOptions)
-{
-	inputOptions.setNormalMap(true);
-	inputOptions.setConvertToNormalMap(false);
-	inputOptions.setGamma(1.0f, 1.0f);
-	inputOptions.setNormalizeMipmaps(true);
-}
-
-///////////////////////////////////////////////////////////////////////////////
-// Set options for color maps.
-///////////////////////////////////////////////////////////////////////////////
-static void setColorMap(nvtt::InputOptions & inputOptions)
-{
-	inputOptions.setNormalMap(false);
-	inputOptions.setConvertToNormalMap(false);
-	inputOptions.setGamma(2.2f, 2.2f);
-	inputOptions.setNormalizeMipmaps(false);
-}
-
-///////////////////////////////////////////////////////////////////////////////
 
 bool NvttCompress( const ork::tool::FilterOptMap& OptionsMap )
 {
-	MyAssertHandler assertHandler;
-	MyMessageHandler messageHandler;
+  std::string tex_in = OptionsMap.GetOption( "-in" )->GetValue();
+  std::string tex_out = OptionsMap.GetOption( "-out" )->GetValue();
+  file::Path inpath( tex_in.c_str() );
+  file::Path outpath( tex_out.c_str() );
 
-	std::string tex_in = OptionsMap.GetOption( "-in" )->GetValue();
-	std::string tex_out = OptionsMap.GetOption( "-out" )->GetValue();
-	file::Path inpath( tex_in.c_str() );
-	file::Path outpath( tex_out.c_str() );
+  /////////////////////////////////////////////////////
+  // implicit output path
+  /////////////////////////////////////////////////////
 
-    printf( "nvtt: converting <%s> to <%s>\n", tex_in.c_str(), tex_out.c_str() );
+  if( tex_out == "" ){
 
-	if( strcmp( inpath.GetExtension().c_str(), "dds" ) == 0 )
-	{
-		const char* frompath = inpath.ToAbsolute().c_str();
-		const char* topath = outpath.ToAbsolute().c_str();
+    printf( "inpath<%s>\n", inpath.c_str() );
 
-		orkprintf( "Copy from<%s> to<%s>\n", frompath, topath );
-		//bool ret = CopyFile( frompath, topath, FALSE );
-		return false; //bool(ret);
-	}
+    std::vector<std::string> out_nodes;
+    SplitString(tex_in, '/',out_nodes);
 
-	nv::Path InNvPath( inpath.ToAbsolute().c_str() );
-	nv::Path OutNvPath( outpath.ToAbsolute().c_str() );
+    auto it = std::find( out_nodes.begin(),
+                   out_nodes.end(),
+                   "textures");
 
-	nvtt::InputOptions inputOptions;
-	inputOptions.setAlphaMode( nvtt::AlphaMode_Transparency );
-
-	nv::Image image;
-	if (!image.load(InNvPath.str()))
-	{
-		printf("ERROR: The file '%s' is not a supported image type.\n", InNvPath.str());
-		return false;
-	}
-
-	bool bflipy = OptionsMap.HasOption( "-flipy" );
-
-	std::string platform_str = OptionsMap.GetOption( "-platform" )->GetValue();
-
-	bool bwii = ( 0 == strcmp( platform_str.c_str(), "wii" ) );
-	bool bxb360 = ( 0 == strcmp( platform_str.c_str(), "xb360" ) );
-
-	bool bnomips = OptionsMap.HasOption( "-nomips" );
-
-	//////////////////////////////////
-	//////////////////////////////////
-	nvtt::CompressionOptions CompressionOptions;
-	CompressionOptions.setFormat(nvtt::Format_DXT1a);
-	//compressionOptions.enableHardwareCompression(true);
-	CompressionOptions.setQuality(nvtt::Quality_Fastest);
-	//////////////////////////////////
-	//////////////////////////////////
-
-	if( bflipy )
-	{
-		int ih = image.height();
-		int iw = image.width();
-
-		nv::Color32* c32ary = new nv::Color32[ iw ];
-
-		int ilinesize = iw*sizeof(nv::Color32);
-
-		int imaxy = (ih>>1);
-
-		for( int iy=0; iy<imaxy; iy++ )
-		{
-			int iothy = ih-(iy+1);
-
-			nv::Color32* scan0 = image.scanline(iy);
-			nv::Color32* scan1 = image.scanline(iothy);
-
-			memcpy( (void*) c32ary, (const void *) scan0, ilinesize );
-			memcpy( (void*) scan0, (const void *) scan1, ilinesize );
-			memcpy( (void*) scan1, (const void *) c32ary, ilinesize );
-		}
-	}
-
-    CompressionOptions.setFormat(nvtt::Format_RGBA);
-
-	//////////////////////////////////
-	// check for full alpha (non compressed)
-	//////////////////////////////////
-    if( 0 )
-    {
-    	bool b_binary_alpha = false;
-
-    	if( image.format() == nv::Image::Format_ARGB )
-    	{
-    		//int iamin = image.GetAlphaMin( );
-    		//int iamax = image.GetAlphaMax( );
-    		//int iaavg = image.GetAlphaAvg( );
-    		//int iaiavg = image.GetAlphaInteriorAvg( );
-
-    		//orkprintf( "INFO: alphas %d %d %d %d\n", iamin, iamax, iaavg, iaiavg );
-
-    		//if( iaavg==0xffffffff )
-    		//{
-    		//	CompressionOptions.setFormat(nvtt::Format_DXT1);
-    		//}
-    		//else if( iaiavg != 0 )
-    		{
-    			//orkprintf( "INFO: non-binary alpha channel found\n" );
-    			b_binary_alpha = false;
-    		//}
-    		//else
-    		//{
-    		//	CompressionOptions.setFormat(nvtt::Format_DXT5);
-    		}
-    	}
-
-    	if( b_binary_alpha )
-    	{
-    		CompressionOptions.setQuantization( true, false, true, 127 );
-    	}
+    if( it != out_nodes.end() ){
+      out_nodes.erase(it);
+      outpath = out_nodes;
+    }
+    it = std::find( out_nodes.begin(),
+                    out_nodes.end(),
+                    "src");
+    if( it != out_nodes.end() ){
+      (*it) = "pc";
+      outpath = out_nodes;
     }
 
-	//////////////////////////////////
-	//////////////////////////////////
+    outpath.SetExtension("dds");
 
-	inputOptions.setTextureLayout(nvtt::TextureType_2D, image.width(), image.height());
-	inputOptions.setMipmapData(image.pixels(), image.width(), image.height());
+    printf( "outpath<%s>\n", outpath.c_str() );
 
-	inputOptions.setMipmapFilter( nvtt::MipmapFilter_Kaiser );
-	inputOptions.setMipmapGeneration(bnomips==false);
+  }
 
-	inputOptions.setRoundMode( nvtt::RoundMode_ToNearestPowerOfTwo );
+  /////////////////////////////////////////////////////
 
-	//inputOptions.setQuantization( true, false, true, 127 );
 
-	//////////////////////////////////
-	// wrap mode (im guessing if the texture is tiled, use Repeat, otherwise use clamp)
+  Spawner s;
+  s.mCommandLine =  "nvcompress ";
+  s.mCommandLine += "-bc3 ";
+  s.mCommandLine += inpath.ToAbsolute().c_str() + std::string(" ");
+  s.mCommandLine += outpath.ToAbsolute().c_str() + std::string(" ");
 
-	//inputOptions.setWrapMode(nvtt::WrapMode_Repeat); // WrapMode_Clamp
-	inputOptions.setWrapMode(nvtt::WrapMode_Repeat); // WrapMode_Clamp
-
-	//if( bwii )
-	//{
-	//	inputOptions.setGamma( 1.0f, 2.0f );
-	//}
-
-	//////////////////////////////////
-
-	setColorMap(inputOptions);
-
-	//////////////////////////////////
-	//////////////////////////////////
-
-	MyErrorHandler errorHandler;
-	nvtt::OutputOptions OutputOptions;
-
-	OutputOptions.setFileName( OutNvPath.str() );
-
-	//////////////////////////////////
-	//////////////////////////////////
-
-	nvtt::Compressor compressor; //  compress(inputOptions, outputOptions, compressionOptions);
-
-    printf( "compressing.....\n");
-	compressor.process( inputOptions, CompressionOptions, OutputOptions );
-
-	//ilSetInteger(IL_DXTC_FORMAT, IL_DXT5);
+  s.spawnSynchronous();
 
 	return true;
 }
 
-#endif
 
 ///////////////////////////////////////////////////////////////////////////////
 }}
