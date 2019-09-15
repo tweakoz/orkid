3d0d4c54928ee42a318e0518f455e3bd4aee9d10
tweakoz <michael@tweakoz.com>
2019-09-15 11:58:22 -0700

    move terrain visualinfo to own data object

--- ork.ent/inc/pkg/ent/HeightFieldDrawable.h
+++ ork.ent/inc/pkg/ent/HeightFieldDrawable.h
@@ -7,22 +7,49 @@ namespace ork::ent {
 ///////////////////////////////////////////////////////////////////////////////
 
 typedef std::shared_ptr<HeightMap> hfptr_t;
+struct HeightFieldDrawable;
+typedef std::shared_ptr<HeightFieldDrawable> hfdrawableptr_t;
+
+class HeightFieldDrawableData : public ork::Object {
+
+	RttiDeclareConcrete( HeightFieldDrawableData, ork::Object );
+
+ public:
+   hfdrawableptr_t createDrawable() const ;
+  HeightFieldDrawableData();
+  ~HeightFieldDrawableData() final;
+
+private:
+
+  friend HeightFieldDrawable;
+
+	void _readEnvMap(ork::rtti::ICastable *&model) const;
+	void _writeEnvMap(ork::rtti::ICastable *const &model);
+  void _writeHmapPath(file::Path const& lmap);
+  void _readHmapPath(file::Path& lmap) const;
+
+  file::Path _hfpath;
+  fvec3 _visualOffset;
+  ork::lev2::textureassetptr_t _sphericalenvmap = nullptr;
+
+};
 
 struct HeightFieldDrawable {
 
-  CallbackDrawable* create();
+  HeightFieldDrawable(const HeightFieldDrawableData& data);
   ~HeightFieldDrawable();
+  CallbackDrawable* create();
 
+  const HeightFieldDrawableData& _data;
   file::Path _hfpath;
   fvec3 _visualOffset;
   float _worldHeight = 0.0f;
   float _worldSizeXZ = 0.0f;
-  ork::lev2::textureassetptr_t _sphericalenvmap;
+  ork::lev2::textureassetptr_t _sphericalenvmap = nullptr;
   CallbackDrawable* _rawdrawable = nullptr;
   ork::svar16_t _impl;
 };
 
-typedef std::shared_ptr<HeightFieldDrawable> hfdrawableptr_t;
 
 ///////////////////////////////////////////////////////////////////////////////
 } //namespace ork::ent {
