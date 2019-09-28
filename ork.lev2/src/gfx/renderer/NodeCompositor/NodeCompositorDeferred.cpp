a249e878eeb8bab23ee4fdb9d5ea511c28f836b0
tweakoz <michael@tweakoz.com>
2019-09-28 04:46:13 -0700

    deferred (WIP)
    Former-commit-id: 58658cf5e60f8e22dbe1ef0bfb5574294d59d3a8

--- ork.lev2/src/gfx/renderer/NodeCompositor/NodeCompositorDeferred.cpp
+++ ork.lev2/src/gfx/renderer/NodeCompositor/NodeCompositorDeferred.cpp
@@ -42,19 +42,24 @@ struct IMPL {
   ///////////////////////////////////////
   void init(lev2::GfxTarget* pTARG) {
     pTARG->debugPushGroup("Deferred::rendeinitr");
-    if (nullptr == _rtg) {
-      _material.Init(pTARG);
-      _rtg            = new RtGroup(pTARG, 8, 8, NUMSAMPLES);
-      auto buf0        = new RtBuffer(_rtg, lev2::ETGTTYPE_MRT0, lev2::EBUFFMT_RGBA32, 8, 8);
-      auto buf1        = new RtBuffer(_rtg, lev2::ETGTTYPE_MRT1, lev2::EBUFFMT_RGBA32, 8, 8);
-      auto buf2        = new RtBuffer(_rtg, lev2::ETGTTYPE_MRT2, lev2::EBUFFMT_RGBA64, 8, 8);
+    if (nullptr == _rtgGbuffer) {
+      _blit2screenmtl.SetUserFx("orkshader://deferred", "deferred_test");
+      _blit2screenmtl.Init(pTARG);
+      _rtgGbuffer            = new RtGroup(pTARG, 8, 8, NUMSAMPLES);
+      auto buf0        = new RtBuffer(_rtgGbuffer, lev2::ETGTTYPE_MRT0, lev2::EBUFFMT_RGBA32, 8, 8);
+      auto buf1        = new RtBuffer(_rtgGbuffer, lev2::ETGTTYPE_MRT1, lev2::EBUFFMT_RGBA32, 8, 8);
+      auto buf2        = new RtBuffer(_rtgGbuffer, lev2::ETGTTYPE_MRT2, lev2::EBUFFMT_RGBA64, 8, 8);
       buf0->_debugName = "DeferredRtAlbAo";
       buf1->_debugName = "DeferredRRufMtl";
       buf2->_debugName = "DeferredRtNormalDist";
-      _rtg->SetMrt(0, buf0);
-      _rtg->SetMrt(1, buf1);
-      _rtg->SetMrt(2, buf2);
-      _effect.PostInit(pTARG, "orkshader://framefx", "frameeffect_standard");
+      _rtgGbuffer->SetMrt(0, buf0);
+      _rtgGbuffer->SetMrt(1, buf1);
+      _rtgGbuffer->SetMrt(2, buf2);
+
+      _rtgLaccum            = new RtGroup(pTARG, 8, 8, NUMSAMPLES);
+      auto bufLA        = new RtBuffer(_rtgLaccum, lev2::ETGTTYPE_MRT0, lev2::EBUFFMT_RGBA64, 8, 8);
+      bufLA->_debugName = "DeferredLightAccum";
+      _rtgLaccum->SetMrt(0, bufLA);
     }
     pTARG->debugPopGroup();
   }
@@ -71,16 +76,19 @@ struct IMPL {
     //////////////////////////////////////////////////////
     int newwidth  = ddprops["OutputWidth"_crcu].Get<int>();
     int newheight = ddprops["OutputHeight"_crcu].Get<int>();
-    if (_rtg->GetW() != newwidth or _rtg->GetH() != newheight) {
-      _rtg->Resize(newwidth, newheight);
+    if (_rtgGbuffer->GetW() != newwidth or _rtgGbuffer->GetH() != newheight) {
+      _rtgGbuffer->Resize(newwidth, newheight);
+      _rtgLaccum->Resize(newwidth,newheight);
+      _width = newwidth;
+      _height = newheight;
     }
     //////////////////////////////////////////////////////
     auto irenderer = ddprops["irenderer"_crcu].Get<lev2::IRenderer*>();
     //////////////////////////////////////////////////////
     targ->debugPushGroup("Deferred::render");
-    RtGroupRenderTarget rt(_rtg);
+    RtGroupRenderTarget rtgbuf(_rtgGbuffer);
     {
-      targ->FBI()->PushRtGroup(_rtg);
+      targ->FBI()->PushRtGroup(_rtgGbuffer);
       targ->FBI()->SetAutoClear(false); // explicit clear
       targ->BeginFrame();
       /////////////////////////////////////////////////////////////////////////////////////////
@@ -88,7 +96,7 @@ struct IMPL {
       auto CPD = CIMPL->topCPD();
       CPD._clearColor = node->_clearColor;
       CPD.mpLayerName = &_layername;
-      CPD._irendertarget = & rt;
+      CPD._irendertarget = & rtgbuf;
       CPD.SetDstRect(tgt_rect);
       CPD._passID = "defgbuffer1"_crcu;
       ///////////////////////////////////////////////////////////////////////////
@@ -113,15 +121,56 @@ struct IMPL {
       /////////////////////////////////////////////////////////////////////////////////////////
       targ->EndFrame();
       targ->FBI()->PopRtGroup();
+      /////////////////////////////////////////////////////////////////////////////////////////
+      // Light Accumulation
+      /////////////////////////////////////////////////////////////////////////////////////////
+      RtGroupRenderTarget rtlaccum(_rtgLaccum);
+      SRect vprect(0, 0, _width, _height);
+      SRect quadrect(0, 0, _width, _height);
+      CPD.SetDstRect(vprect);
+      CPD._irendertarget = & rtlaccum;
+      CPD._cameraMatrices = nullptr;
+      CPD._stereoCameraMatrices = nullptr;
+      CIMPL->pushCPD(CPD);
+      targ->debugPushGroup("PtxCompositingNode::to_output");
+      targ->FBI()->SetAutoClear(false);
+      targ->FBI()->PushRtGroup(_rtgLaccum);
+      targ->BeginFrame();
+      targ->FBI()->Clear(fvec4(0.1,0.2,0.3,1), 1.0f);
+      auto this_buf = targ->FBI()->GetThisBuffer();
+      fvec4 color(1.0f, 1.0f, 1.0f, 1.0f);
+      _blit2screenmtl.SetAuxMatrix(fmtx4::Identity);
+      _blit2screenmtl.SetTexture(_rtgGbuffer->GetMrt(0)->GetTexture());
+      _blit2screenmtl.SetTexture2(_rtgGbuffer->GetMrt(1)->GetTexture());
+      _blit2screenmtl.SetTexture3(_rtgGbuffer->GetMrt(2)->GetTexture());
+      _blit2screenmtl.SetColorMode(GfxMaterial3DSolid::EMODE_USER);
+      _blit2screenmtl.mRasterState.SetBlending(EBLENDING_OFF);
+      _blit2screenmtl.mRasterState.SetDepthTest(EDEPTHTEST_OFF);
+      this_buf->RenderMatOrthoQuad(vprect,
+                                   quadrect,
+                                   &_blit2screenmtl,
+                                   0.0f,
+                                   1.0f, // u0 v0
+                                   1.0f,
+                                   0.0f, // u1 v1
+                                   nullptr,
+                                   color);
+      targ->EndFrame();
+      targ->FBI()->PopRtGroup();
+      targ->debugPopGroup();
+      CIMPL->popCPD();
+      /////////////////////////////////////////////////////////////////////////////////////////
     }
     targ->debugPopGroup();
   }
   ///////////////////////////////////////
   PoolString _camname, _layername;
-  CompositingMaterial _material;
-  RtGroup* _rtg = nullptr;
-  BuiltinFrameEffectMaterial _effect;
+  ork::lev2::GfxMaterial3DSolid _blit2screenmtl;
+  RtGroup* _rtgGbuffer = nullptr;
+  RtGroup* _rtgLaccum = nullptr;
   fmtx4 _viewOffsetMatrix;
+  int _width = 0;
+  int _height = 0;
 };
 } // namespace deferrednode
 
@@ -142,7 +191,7 @@ void DeferredCompositingNode::DoRender(CompositorDrawData& drawdata) {
 RtBuffer* DeferredCompositingNode::GetOutput() const {
   static int i = 0;
   i++;
-return _impl.Get<std::shared_ptr<deferrednode::IMPL>>()->_rtg->GetMrt(((i>>10)&7)%3);
+return _impl.Get<std::shared_ptr<deferrednode::IMPL>>()->_rtgLaccum->GetMrt(0);
 }
 ///////////////////////////////////////////////////////////////////////////////
 }} // namespace ork::lev2
