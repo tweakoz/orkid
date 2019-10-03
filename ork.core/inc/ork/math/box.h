a990d062c511159d6081daac8cbed923ac34bcf8
Michael T. Mayers <michael@tweakoz.com>
2019-10-02 21:04:27 -0700

    prep for lightmerging
    Former-commit-id: c0b160da6fb86615a943bc0f9220d94cd1e40810

--- ork.core/inc/ork/math/box.h
+++ ork.core/inc/ork/math/box.h
@@ -3,9 +3,9 @@
 // Copyright 1996-2012, Michael T. Mayers.
 // Distributed under the Boost Software License - Version 1.0 - August 17, 2003
 // see http://www.boost.org/LICENSE_1_0.txt
-//////////////////////////////////////////////////////////////// 
+////////////////////////////////////////////////////////////////
 
-#pragma once 
+#pragma once
 
 #include <ork/math/plane.h>
 #include <ork/object/Object.h>
@@ -14,52 +14,48 @@
 namespace ork {
 ///////////////////////////////////////////////////////////////////////////////
 
-class AABox : public ork::Object
-{
-	RttiDeclareConcrete(AABox,ork::Object);
+class AABox : public ork::Object {
+  RttiDeclareConcrete(AABox, ork::Object);
 
-	fvec3	mMin;
-	fvec3	mMax;
+  fvec3 mMin;
+  fvec3 mMax;
 
-	fplane3		mPlaneNX[2];
-	fplane3		mPlaneNY[2];
-	fplane3		mPlaneNZ[2];
+  fplane3 mPlaneNX[2];
+  fplane3 mPlaneNY[2];
+  fplane3 mPlaneNZ[2];
 
-	void ComputePlanes();
+  void ComputePlanes();
 
 public:
+  void SupportMapping(const fvec3 &v, fvec3 &result) const;
 
-    void SupportMapping( const fvec3& v, fvec3& result) const;
+  fvec3 Corner(int n) const;
 
-	fvec3 Corner(int n) const;
+  bool Intersect(const fray3 &ray, fvec3 &isect_in, fvec3 &isect_out) const;
+  bool Contains(const fvec3 &test_point) const;
+  bool Contains(const float test_point_X, const float test_point_Z) const;
+  void Constrain(float &test_point_X, float &test_point_Z) const;
+  void Constrain(fvec3 &test_point) const;
 
-	bool Intersect( const fray3& ray, fvec3& isect_in, fvec3& isect_out ) const;
-	bool Contains(const fvec3& test_point) const;
-	bool Contains(const float test_point_X, const float test_point_Z) const;
-    void Constrain(float &test_point_X, float &test_point_Z) const;
-    void Constrain(fvec3& test_point) const;
+  AABox();
+  AABox(const fvec3 &vmin, const fvec3 &vmax);
+  AABox(const AABox &oth);
+  void operator=(const AABox &oth);
 
-    AABox();
-    AABox( const fvec3& vmin, const fvec3& vmax );
-    AABox( const AABox& oth );
-    void operator=(const AABox& oth );
-    
-	inline const fvec3& Min() const { return mMin; }
-	inline const fvec3& Max() const { return mMax; }
-    inline fvec3 GetSize() const { return Max()-Min(); }
+  inline const fvec3 &Min() const { return mMin; }
+  inline const fvec3 &Max() const { return mMax; }
+  inline fvec3 GetSize() const { return Max() - Min(); }
 
-	void BeginGrow();
-	void Grow( const fvec3& vin );
-	void EndGrow();
+  void BeginGrow();
+  void Grow(const fvec3 &vin);
+  void EndGrow();
 
-	void SetMinMax( const fvec3& vmin, const fvec3& vmax );
+  void SetMinMax(const fvec3 &vmin, const fvec3 &vmax);
 
 private:
-	bool PostDeserialize(reflect::IDeserializer &) final;
-
+  bool PostDeserialize(reflect::IDeserializer &) final;
 };
 
 ///////////////////////////////////////////////////////////////////////////////
-}
+} // namespace ork
 ///////////////////////////////////////////////////////////////////////////////
-
--- ork.core/inc/ork/math/sphere.h
+++ ork.core/inc/ork/math/sphere.h
@@ -7,6 +7,8 @@
 
 #pragma once
 
+#include <ork/math/box.h>
+#include <ork/math/cmatrix4.h>
 #include <ork/math/cvector3.h>
 #include <ork/math/line.h>
 
@@ -18,19 +20,22 @@ struct Sphere {
   fvec3 mCenter;
   float mRadius;
 
-  Sphere(const fvec3& pos, float r) : mCenter(pos), mRadius(r) {}
-  Sphere(const fvec3& boxmin, const fvec3& boxmax);
+  Sphere(const fvec3 &pos, float r) : mCenter(pos), mRadius(r) {}
+  Sphere(const fvec3 &boxmin, const fvec3 &boxmax);
 
-  void SupportMapping(const fvec3& v, fvec3& result) const;
+  void SupportMapping(const fvec3 &v, fvec3 &result) const;
 
-  bool Intersect(const fray3& ray, fvec3& isect_in, fvec3& isect_out, fvec3& sphnormal) const;
+  bool Intersect(const fray3 &ray, fvec3 &isect_in, fvec3 &isect_out,
+                 fvec3 &sphnormal) const;
+  // AABox2D projectedBounds(const fmtx4& mvp) const;
 };
 
 ///////////////////////////////////////////////////////////////////////////////
 
-inline void Sphere::SupportMapping(const fvec3& v, fvec3& result) const {}
+inline void Sphere::SupportMapping(const fvec3 &v, fvec3 &result) const {}
 
-inline bool Sphere::Intersect(const fray3& ray, fvec3& isect_in, fvec3& isect_out, fvec3& isect_normal) const {
+inline bool Sphere::Intersect(const fray3 &ray, fvec3 &isect_in,
+                              fvec3 &isect_out, fvec3 &isect_normal) const {
   fvec3 L = mCenter - ray.mOrigin;
   float tca = L.Dot(ray.mDirection);
   if (tca < 0.0f)
@@ -47,6 +52,30 @@ inline bool Sphere::Intersect(const fray3& ray, fvec3& isect_in, fvec3& isect_ou
 }
 
 ///////////////////////////////////////////////////////////////////////////////
+/*
+AABox Sphere::projectedBounds(const fmtx4& mvp) const {
+
+    float d2 = mCenter.Dot(mCenter);
+    float a = 1.0f/sqrt(d2 - mRadius * mRadius);
+    fvec3 right = (mRadius*a) * vec3(-mCenter.z, 0, mCenter.x);
+    fvec3 up = fvec3(0,mRadius,0);
+
+    fvec4 ctr = fvec4(center,1).Transform(mvp);
+    fvec4 right  = fvec4(right,0).Transform(mvp);
+    fvec4 up     = fvec4(up,0).Transform(mvp);
+
+    AABox2D rval;
+    rval.BeginGrow();
+      rval.Grow((ctr+up).PerspectiveDivide().xyz());
+      rval.Grow((ctr+right).PerspectiveDivide().xyz());
+      rval.Grow((ctr-up).PerspectiveDivide().xyz());
+      rval.Grow((ctr-right).PerspectiveDivide().xyz());
+    rval.EndGrow();
+
+    return rval;
+}*/
+
+///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -54,7 +83,7 @@ struct Circle {
   fvec2 mCenter;
   float mRadius;
 
-  Circle(const fvec2& pos, float r) : mCenter(pos), mRadius(r) {}
+  Circle(const fvec2 &pos, float r) : mCenter(pos), mRadius(r) {}
 };
 
 //////////////////////////
