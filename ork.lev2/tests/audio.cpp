d9669e882c7629d9256c98fa6067ce45dd17a501
Michael T. Mayers <michael@tweakoz.com>
2019-11-22 14:04:34 -0800

    lev2 tests (WIP)
    Former-commit-id: b8dfc9610457169a8a05201717d8b6515a784f69

---
+++ ork.lev2/tests/audio.cpp
@@ -0,0 +1,10 @@
+#include <ork/pch.h>
+#include <ork/lev2/aud/audiodevice.h>
+#include <utpp/UnitTest++.h>
+
+
+TEST(Audio1) {
+  ork::lev2::AudioDevice* paudio = ork::lev2::AudioDevice::GetDevice();
+  printf( "audiodev<%p>\n", paudio );
+  CHECK(paudio!=nullptr);
+}
