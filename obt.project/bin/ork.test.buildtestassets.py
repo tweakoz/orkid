a03b474371ac5a21da7522638125913737364584
Michael T. Mayers <michael@tweakoz.com>
2020-01-20 03:05:52 -0800

    fix asset paths

--- obt.project/bin/ork.test.buildtestassets.py
+++ obt.project/bin/ork.test.buildtestassets.py
@@ -9,6 +9,11 @@ base = Path(os.environ["ORKID_WORKSPACE_DIR"])
 
 tool = "ork.tool.release"
 
+def mkdir( dirname ):
+    cmd = ["mkdir","-p",base/"ork.data"/"pc"/"test"/dirname]
+    ork.command.run(cmd)
+    print(cmd)
+
 def exp_anim( name ):
     src = base/"ork.data"/"src"/"test"/name
     dst = (base/"ork.data"/"pc"/"test"/name).with_suffix('.xga')
@@ -22,6 +27,9 @@ def exp_mesh( name ):
     cmd = [tool,"--filter","ass:xgm", "--in",src, "--out", dst]
     ork.command.run(cmd)
     print(cmd)
+
+mkdir("chartest")
+mkdir("hfstest")
 
 animlist = ["bonetest_anim.dae","rigtest_anim.gltf","chartest/char_anim.gltf","hfstest/hfs_rigtest_anim.fbx"]
 for item in animlist:
