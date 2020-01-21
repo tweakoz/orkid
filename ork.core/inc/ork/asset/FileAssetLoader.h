1c03b1b69984c336f222f78bdfd4ed0d0f79a2c7
Michael T. Mayers <michael@tweakoz.com>
2020-01-21 00:14:42 -0800

    fix build issue on other system

--- ork.core/inc/ork/asset/FileAssetLoader.h
+++ ork.core/inc/ork/asset/FileAssetLoader.h
@@ -5,45 +5,45 @@
 #pragma once
 
 #include <ork/asset/AssetLoader.h>
-#include <ork/orkstl.h> // for orkvector
+#include <ork/orkstl.h>                      // for orkvector
 #include <ork/kernel/string/MutableString.h> // for orkvector
 #include <ork/kernel/fixedstring.h>
 
 namespace ork {
 class PieceString;
 class MutableString;
-};
+}; // namespace ork
 
 namespace ork { namespace asset {
 
 class Asset;
+class AssetClass;
 
 typedef fxstring<8> file_ext_t;
 typedef fxstring<32> file_loc_t;
 typedef file::Path file_pathbase_t;
 
-struct FileSet
-{
-	file_ext_t mExt;
-	file_loc_t mLoc;
-	file_pathbase_t mPathBase;
+struct FileSet {
+  file_ext_t mExt;
+  file_loc_t mLoc;
+  file_pathbase_t mPathBase;
 };
 
-struct FileAssetLoader : public AssetLoader
-{
-	FileAssetLoader(const AssetClass* ac) : mAssetClass(ac) {}
-	bool FindAsset(const PieceString &, MutableString result, int first_extension = 0);
-	bool CheckAsset(const PieceString &) override;
-	bool LoadAsset(Asset *asset) override;
-	void AddLocation( file_pathbase_t b, file_ext_t e );
+struct FileAssetLoader : public AssetLoader {
+  FileAssetLoader(const AssetClass* ac)
+      : mAssetClass(ac) {
+  }
+  bool FindAsset(const PieceString&, MutableString result, int first_extension = 0);
+  bool CheckAsset(const PieceString&) override;
+  bool LoadAsset(Asset* asset) override;
+  void AddLocation(file_pathbase_t b, file_ext_t e);
 
 protected:
+  virtual bool LoadFileAsset(Asset* asset, ConstString filename) = 0;
+  std::set<file::Path> EnumerateExisting() override;
 
-	virtual bool LoadFileAsset(Asset *asset, ConstString filename) = 0;
-	std::set<file::Path> EnumerateExisting() override;
-
-	const AssetClass* mAssetClass;
-	std::vector<FileSet> mLocations;
+  const AssetClass* mAssetClass;
+  std::vector<FileSet> mLocations;
 };
 
-} }
+}} // namespace ork::asset
