9b49c14c84382cb43f6b9e815d758e07c494960c
tweakoz <michael@tweakoz.com>
2020-01-29 00:44:10 -0800

    osx sync

--- build.py
+++ build.py
@@ -61,7 +61,7 @@ cmd += ["-DCMAKE_FIND_DEBUG_MODE=ON","--target","install"]
 
 cmd += [prj_root]
 
-ork.dep.require(["yarl","bullet","luajit","openexr","oiio","openvr","fcollada","assimp","nvtt"])
+ork.dep.require(["yarl","bullet","luajit","openexr","oiio","openvr","fcollada","assimp","nvtt","lua"])
 
 Command(cmd).exec()
 
