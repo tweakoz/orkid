a7327d9e371c474652f7610cb85ab63ad9de51d7
Michael T. Mayers <michael@tweakoz.com>
2020-03-25 13:20:23 -0700

    ..

--- ork.core/inc/ork/kernel/fixedstring.hpp
+++ ork.core/inc/ork/kernel/fixedstring.hpp
@@ -1,2 +1,2 @@
 #pragma once
-#include "tempstring.hpp"
+#include <ork/kernel/tempstring.hpp>
--- ork.core/inc/ork/kernel/semaphore.h
+++ ork.core/inc/ork/kernel/semaphore.h
@@ -1,39 +1,35 @@
 #pragma once
 
 #include <ork/kernel/atomic.h>
-#include "mutex.h"
+#include <ork/kernel/mutex.h>
 #include <pthread.h>
 
 namespace ork {
 
-struct condition_variable
-{
-    condition_variable(bool do_init = false);
+struct condition_variable {
+  condition_variable(bool do_init = false);
 
-    void init();
-    void notify_one();
-    void notify_all();
-    int wait(unsigned long usec);
-
-private:
-
-    pthread_mutex_t mMutex;
-    pthread_cond_t  mCondVar;
-    ork::atomic<int64_t> mWaitCount;
-    ork::atomic<int64_t> mReleaseCount;
-};
-
-struct semaphore
-{
-    semaphore(const char* name);
-    void notify();
-    void wait();
+  void init();
+  void notify_one();
+  void notify_all();
+  int wait(unsigned long usec);
 
 private:
-    ork::mutex mMutex;
-    std::condition_variable mCondition;
-    ork::atomic<int> mCount;
+  pthread_mutex_t mMutex;
+  pthread_cond_t mCondVar;
+  ork::atomic<int64_t> mWaitCount;
+  ork::atomic<int64_t> mReleaseCount;
 };
 
+struct semaphore {
+  semaphore(const char* name);
+  void notify();
+  void wait();
+
+private:
+  ork::mutex mMutex;
+  std::condition_variable mCondition;
+  ork::atomic<int> mCount;
+};
 
 } // namespace ork
