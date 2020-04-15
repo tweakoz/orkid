170d80efa3c1020732bb1d74091c79b1efc95d22
Michael T. Mayers <michael@tweakoz.com>
2020-04-14 21:13:17 -0700

    ..

--- ork.lev2/inc/ork/lev2/gfx/meshutil/meshutil.h
+++ ork.lev2/inc/ork/lev2/gfx/meshutil/meshutil.h
@@ -304,38 +304,6 @@ private:
 };
 
 ///////////////////////////////////////////////////////////////////////////////
-/*
-struct TriStripperPrimGroup
-{
-    orkvector<unsigned int> mIndices;
-};
-
-class TriStripper
-{
-    triangle_stripper::tri_stripper tristripper;
-    orkvector<TriStripperPrimGroup> mStripGroups;
-    TriStripperPrimGroup			mTriGroup;
-
-public:
-
-    TriStripper( const std::vector<unsigned int> &InTriIndices, int icachesize, int iminstripsize );
-
-    const orkvector<TriStripperPrimGroup> & GetStripGroups( void ) const
-    {
-        return mStripGroups;
-    }
-
-    const orkvector<unsigned int> & GetStripIndices( int igroup ) const
-    {
-        return mStripGroups[igroup].mIndices;
-    }
-
-    const orkvector<unsigned int> & GetTriIndices( void ) const { return mTriGroup.mIndices; }
-
-};
-*/
-
-///////////////////////////////////////////////////////////////////////////////
 
 class AmbientLight : public Light {
   RttiDeclareConcrete(AmbientLight, Light);
