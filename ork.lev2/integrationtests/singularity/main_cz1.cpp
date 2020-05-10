1cf60cfe3a779eda11c95ff8dabf10a5c952e248
Michael T. Mayers <michael@tweakoz.com>
2020-05-10 13:14:27 -0700

    bootstrap hud UI for cz1 test

--- ork.lev2/integrationtests/singularity/main_cz1.cpp
+++ ork.lev2/integrationtests/singularity/main_cz1.cpp
@@ -1,4 +1,5 @@
 #include <QWindow>
+#include <portaudio.h>
 #include <ork/application/application.h>
 #include <ork/kernel/string/deco.inl>
 #include <ork/kernel/timer.h>
@@ -8,7 +9,8 @@
 #include <ork/lev2/aud/singularity/krzdata.h>
 #include <ork/lev2/aud/singularity/synth.h>
 #include <ork/lev2/aud/singularity/krzobjects.h>
-#include <portaudio.h>
+#include <ork/lev2/gfx/renderer/drawable.h>
+#include <ork/lev2/gfx/material_freestyle.h>
 
 using namespace std::string_literals;
 using namespace ork;
@@ -27,6 +29,63 @@ int main(int argc, char** argv) {
   auto basepath = file::Path::share_dir() / "singularity" / "casioCZ";
   startupAudio();
   the_synth->resetFenables();
+  //////////////////////////////////////////////////////////////////////////////
+  // boot up debug HUD
+  //////////////////////////////////////////////////////////////////////////////
+  auto qtapp  = OrkEzQtApp::create(argc, argv);
+  auto qtwin  = qtapp->_mainWindow;
+  auto gfxwin = qtwin->_gfxwin;
+  FreestyleMaterial material;
+  const FxShaderTechnique* fxtechnique = nullptr;
+  const FxShaderParam* fxparameterMVP  = nullptr;
+  const FxShaderParam* fxparameterMODC = nullptr;
+  //////////////////////////////////////////////////////////
+  qtapp->onGpuInit([&](Context* ctx) {
+    material.gpuInit(ctx, "orkshader://solid");
+    fxtechnique     = material.technique("mmodcolor");
+    fxparameterMVP  = material.param("MatMVP");
+    fxparameterMODC = material.param("modcolor");
+    deco::printf(fvec3::White(), "gpuINIT - context<%p>\n", ctx, fxtechnique);
+    deco::printf(fvec3::Yellow(), "  fxtechnique<%p>\n", fxtechnique);
+    deco::printf(fvec3::Yellow(), "  fxparameterMVP<%p>\n", fxparameterMVP);
+    deco::printf(fvec3::Yellow(), "  fxparameterMODC<%p>\n", fxparameterMODC);
+  });
+  //////////////////////////////////////////////////////////
+  qtapp->onDraw([&](ui::drawevent_constptr_t drwev) {
+    auto context        = drwev->GetTarget();
+    auto fbi            = context->FBI(); // FrameBufferInterface
+    auto fxi            = context->FXI(); // FX Interface
+    float r             = float(rand() % 256) / 255.0f;
+    float g             = float(rand() % 256) / 255.0f;
+    float b             = float(rand() % 256) / 255.0f;
+    int TARGW           = context->mainSurfaceWidth();
+    int TARGH           = context->mainSurfaceHeight();
+    const SRect tgtrect = SRect(0, 0, TARGW, TARGH);
+
+    fbi->SetClearColor(fvec4(r, g, b, 1));
+    context->beginFrame();
+    RenderContextFrameData RCFD(context);
+    material.begin(fxtechnique, RCFD);
+    material.bindParamMatrix(fxparameterMVP, fmtx4::Identity());
+    material.bindParamVec4(fxparameterMODC, fvec4::Red());
+    gfxwin->Render2dQuadEML(fvec4(-0.5, -0.5, 1, 1), fvec4(0, 0, 1, 1), fvec4(0, 0, 1, 1));
+    material.end(RCFD);
+    context->endFrame();
+  });
+  //////////////////////////////////////////////////////////
+  qtapp->onResize([&](int w, int h) { printf("GOTRESIZE<%d %d>\n", w, h); });
+  //////////////////////////////////////////////////////////
+  qtapp->onUiEvent([&](ui::event_constptr_t ev) -> ui::HandlerResult {
+    switch (ev->mEventCode) {
+      case ui::UIEV_DOUBLECLICK:
+        OrkAssert(false);
+        break;
+      default:
+        break;
+    }
+    ui::HandlerResult rval;
+    return rval;
+  });
   //////////////////////////////////////////////////////////////////////////////
   auto add_event = [&](const programData* prog, //
                        float time,
@@ -50,7 +109,6 @@ int main(int argc, char** argv) {
       auto prg = czdata->getProgram(0);
       add_event(prg, float(0) * 2, 4.0, 36 + i * 12);
     }
-    usleep(35 << 20); // just wait, let the "music" play..
   } else {
     czdata->loadBank(basepath / "factoryA.bnk", "bank1");
     czdata->loadBank(basepath / "factoryB.bnk", "bank2");
@@ -59,9 +117,9 @@ int main(int argc, char** argv) {
       printf("i<%d> prg<%p>\n", i, prg);
       add_event(prg, float(i) * 0.5, 1.0, 36);
     }
-    usleep(35 << 20); // just wait, let the "music" play..
   }
   //////////////////////////////////////////////////////////////////////////////
+  qtapp->exec();
   tearDownAudio();
   return 0;
 }
