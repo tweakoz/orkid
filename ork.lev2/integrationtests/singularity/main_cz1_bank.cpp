7242e82fc63b6a0f686da5e592e4af0a7ad74818
Michael T. Mayers <michael@tweakoz.com>
2020-05-11 12:01:34 -0700

    remove testprogram code from cz1_bank

--- ork.lev2/integrationtests/singularity/main_cz1_bank.cpp
+++ ork.lev2/integrationtests/singularity/main_cz1_bank.cpp
@@ -6,20 +6,12 @@ int main(int argc, char** argv) {
   startupAudio();
   //////////////////////////////////////////////////////////////////////////////
   auto czdata = std::make_shared<CzData>(the_synth);
-  bool TEST   = true;
-  if (TEST) {
-    // czdata->loadBank(basepath / "edit.syx", "bank1");
-    czdata->loadBank(basepath / "factoryA.bnk", "bank1");
-    auto prg = czdata->getProgram(0);
-    enqueue_audio_event(prg, float(0) * 2, 60.0, 48);
-  } else {
-    czdata->loadBank(basepath / "factoryA.bnk", "bank1");
-    czdata->loadBank(basepath / "factoryB.bnk", "bank2");
-    for (int i = 0; i < 64; i++) { // 2 32 patch banks
-      auto prg = czdata->getProgram(i);
-      printf("i<%d> prg<%p>\n", i, prg);
-      enqueue_audio_event(prg, float(i) * 0.5, 1.0, 44 - 12);
-    }
+  czdata->loadBank(basepath / "factoryA.bnk", "bank1");
+  czdata->loadBank(basepath / "factoryB.bnk", "bank2");
+  for (int i = 0; i < 64; i++) { // 2 32 patch banks
+    auto prg = czdata->getProgram(i);
+    printf("i<%d> prg<%p>\n", i, prg);
+    enqueue_audio_event(prg, float(i) * 0.5, 1.0, 44 - 12);
   }
   //////////////////////////////////////////////////////////////////////////////
   qtapp->setRefreshPolicy({EREFRESH_FASTEST, 0});
