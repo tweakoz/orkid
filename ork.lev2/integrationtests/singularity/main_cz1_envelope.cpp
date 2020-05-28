3f6783e149c08419613423a1c8e08c5b7de28e61
Michael T. Mayers <michael@tweakoz.com>
2020-05-28 10:33:12 -0700

    ..

--- ork.lev2/integrationtests/singularity/main_cz1_envelope.cpp
+++ ork.lev2/integrationtests/singularity/main_cz1_envelope.cpp
@@ -9,26 +9,30 @@ int main(int argc, char** argv) {
   auto qtapp = createEZapp(argc, argv);
   startupAudio();
   //////////////////////////////////////////////////////////////////////////////
-  // allocate components
+  // allocate program/layer data
   //////////////////////////////////////////////////////////////////////////////
   auto program   = std::make_shared<ProgramData>();
   auto layerdata = program->newLayer();
   auto czoscdata = std::make_shared<CzOscData>();
-  auto DCAENV    = layerdata->appendController<RateLevelEnvData>("DCAENV");
-  auto DCWENV    = layerdata->appendController<RateLevelEnvData>("DCWENV");
-  auto LFO2      = layerdata->appendController<LfoData>("MYLFO2");
-  auto LFO1      = layerdata->appendController<LfoData>("MYLFO1");
-  //////////////////////////////////////
-  // set names
-  //////////////////////////////////////
   program->_role = "czx";
   program->_name = "test";
   //////////////////////////////////////
-  // create layer
+  // setup dsp graph
   //////////////////////////////////////
-  layerdata->_algdata = configureKrzAlgorithm(1);
+  layerdata->_algdata = configureCz1Algorithm(1);
+  auto dcostage       = layerdata->stageByName("DCO");
+  auto ampstage       = layerdata->stageByName("AMP");
+  auto osc            = dcostage->appendTypedBlock<CZX>(czoscdata, 0);
+  auto amp            = ampstage->appendTypedBlock<AMP>();
   //////////////////////////////////////
-  // set envelope
+  // setup modulators
+  //////////////////////////////////////
+  auto DCAENV = layerdata->appendController<RateLevelEnvData>("DCAENV");
+  auto DCWENV = layerdata->appendController<RateLevelEnvData>("DCWENV");
+  auto LFO2   = layerdata->appendController<LfoData>("MYLFO2");
+  auto LFO1   = layerdata->appendController<LfoData>("MYLFO1");
+  //////////////////////////////////////
+  // setup envelope
   //////////////////////////////////////
   DCAENV->_ampenv = true;
   DCAENV->_segments.push_back({.2, .7});  // atk1
@@ -45,7 +49,7 @@ int main(int argc, char** argv) {
   DCWENV->_segments.push_back({40, 1});   // rel2
   DCWENV->_segments.push_back({40, 0});   // rel3
   //////////////////////////////////////
-  // set LFO
+  // setup LFO
   //////////////////////////////////////
   LFO1->_minRate = 0.25;
   LFO1->_maxRate = 0.25;
@@ -54,13 +58,9 @@ int main(int argc, char** argv) {
   LFO2->_maxRate = 3.3;
   LFO2->_shape   = "Sine";
   //////////////////////////////////////
-  // setup dsp graph
+  // setup modulation routing
   //////////////////////////////////////
-  auto osc = layerdata->stageByName("DCO")->appendBlock();
-  auto amp = layerdata->stageByName("AMP")->appendBlock();
-  CZX::initBlock(osc, czoscdata, 0);
-  AMP::initBlock(amp);
-  auto& modulation_index_param      = osc->_paramd[1]._mods;
+  auto& modulation_index_param      = osc->_paramd[0]._mods;
   modulation_index_param._src1      = DCWENV;
   modulation_index_param._src1Depth = 1.0;
   // modulation_index_param._src2      = LFO1;
@@ -72,15 +72,17 @@ int main(int argc, char** argv) {
   czoscdata->_dcoBaseWaveB = 7;
   czoscdata->_dcoWindow    = 2;
   //////////////////////////////////////
-  auto& amp_param   = amp->addParam();
+  auto& amp_param   = amp->_paramd[0];
   amp_param._coarse = 0.0f;
   amp_param.useDefaultEvaluator();
   amp_param._mods._src1      = DCAENV;
   amp_param._mods._src1Depth = 1.0;
   //////////////////////////////////////
-  // play a test note
+  // play test notes
   //////////////////////////////////////
   enqueue_audio_event(program.get(), 1.5f, 240.0, 48);
+  //////////////////////////////////////////////////////////////////////////////
+  // test harness UI
   //////////////////////////////////////////////////////////////////////////////
   qtapp->setRefreshPolicy({EREFRESH_FASTEST, 0});
   qtapp->exec();
