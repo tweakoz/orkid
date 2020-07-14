95724ab580d00f65ba6bebdbf7e1d89b54ac82a6
Michael T. Mayers <michael@tweakoz.com>
2020-07-14 01:05:58 -0700

    store vcd samples

--- ork.core/inc/ork/hdl/vcd.h
+++ ork.core/inc/ork/hdl/vcd.h
@@ -2,16 +2,31 @@
 
 #include <ork/file/path.h>
 #include <ork/util/scanner.h>
+#include <unordered_map>
 
 namespace ork::hdl::vcd {
 
 struct Signal;
 struct Scope;
 struct File;
+struct Sample;
 
 using signal_ptr_t = std::shared_ptr<Signal>;
 using scope_ptr_t  = std::shared_ptr<Scope>;
+using sample_ptr_t = std::shared_ptr<Sample>;
 using file_ptr_t   = std::shared_ptr<File>;
+
+constexpr size_t kmaxbitlen = 256;
+struct Sample {
+  Sample() {
+    _packedbits[0] = 0;
+    _packedbits[1] = 0;
+    _packedbits[2] = 0;
+    _packedbits[3] = 0;
+  }
+  uint64_t _packedbits[4];
+  int _numbits = 0;
+};
 
 struct Signal {
 
@@ -20,7 +35,7 @@ struct Signal {
   std::string _longname  = "";
   int _bit_width         = 0;
   int _word_width        = 0;
-  std::vector<uint64_t> _storage;
+  std::unordered_map<uint64_t, sample_ptr_t> _unsorted_samples;
 };
 
 struct Scope {
--- ork.core/src/hdl/vcd.cpp
+++ ork.core/src/hdl/vcd.cpp
@@ -76,6 +76,9 @@ void File::parse(ork::file::Path& inppath) {
   std::map<std::string, std::string> varmap;
   ////////////////////////////////////
   ParseState parse_state = ParseState::INIT;
+  size_t cur_timestamp   = 0;
+  ////////////////////////////////////
+  Sample cursample;
   ////////////////////////////////////
   size_t index = 0;
   while (index < numtoks) {
@@ -126,10 +129,11 @@ void File::parse(ork::file::Path& inppath) {
           sig->_longname   = varlong.text;
           sig->_type       = vartype.text;
           sig->_bit_width  = atoi(varwidth.text.c_str());
-          sig->_word_width = sig->_bit_width >> 3;
+          sig->_word_width = sig->_bit_width >> 6;
 
           _signals_by_shortname[varshort.text] = sig;
 
+          OrkAssert(sig->_bit_width <= kmaxbitlen);
         }
         ///////////////////////////////
         else if (toktext == "$enddefinitions") {
@@ -160,6 +164,10 @@ void File::parse(ork::file::Path& inppath) {
             OrkAssert(it != _signals_by_shortname.end());
             auto sig = it->second;
             printf("index<%zu> SIG<%s:%d:%s>\n", index, sig->_type.c_str(), sig->_bit_width, sig->_longname.c_str());
+
+            auto copyofsample                     = std::make_shared<Sample>(cursample);
+            sig->_unsorted_samples[cur_timestamp] = copyofsample;
+
             parse_state = ParseState::VALUE;
             break;
           }
@@ -172,12 +180,17 @@ void File::parse(ork::file::Path& inppath) {
 
             if (starts_bool) {
               printf("mushed VALUE: %c\n", toktext[0]);
+              cursample          = Sample();
+              cursample._numbits = 1;
+              cursample._packedbits[3] |= (toktext[0] == '1');
               auto key = toktext.substr(1);
               auto it  = _signals_by_shortname.find(key);
               OrkAssert(it != _signals_by_shortname.end());
               auto sig = it->second;
               printf("mushed SIG<%s:%d:%s>\n", sig->_type.c_str(), sig->_bit_width, sig->_longname.c_str());
-              parse_state = ParseState::VALUE;
+              auto copyofsample                     = std::make_shared<Sample>(cursample);
+              sig->_unsorted_samples[cur_timestamp] = copyofsample;
+              parse_state                           = ParseState::VALUE;
             } else {
               OrkAssert(false);
             }
@@ -194,16 +207,40 @@ void File::parse(ork::file::Path& inppath) {
       }
       case TokenClass::BITVECTOR: {
         OrkAssert(parse_state == ParseState::VALUE);
-        printf("index<%zu> BITVECTOR<%s>\n", index, toktext.c_str());
+        int numbitsinsample = toktext.length() - 1;
+        printf("index<%zu> BITVECTOR<%d:%s>\n", index, numbitsinsample, toktext.c_str());
+        cursample          = Sample();
+        cursample._numbits = numbitsinsample;
+        for (int ibit = 0; ibit < numbitsinsample; ibit++) {
+          char bitval = toktext[ibit + 1];
+          switch (bitval) {
+            case '0':
+              break;
+            case '1': {
+              int abit = (numbitsinsample - 1) - ibit;
+              int awrd = abit >> 6;
+              cursample._packedbits[awrd] |= (1 << abit);
+              break;
+            }
+            default:
+              OrkAssert(false);
+              break;
+          }
+        }
+
         index++;
         parse_state = ParseState::KEY;
         break;
       }
-      case TokenClass::TIMESTAMP:
-        printf("index<%zu> TIMESTAMP<%s>\n", index, toktext.c_str());
-        index += 2;
+      case TokenClass::TIMESTAMP: {
+        auto timestr  = toktext.substr(1);
+        auto timeval  = size_t(atol(timestr.c_str()));
+        cur_timestamp = timeval;
+        printf("index<%zu> TIMESTAMP<%zu>\n", index, cur_timestamp);
         parse_state = ParseState::VALUE;
+        index++;
         break;
+      }
       default:
         OrkAssert(false);
         break;
