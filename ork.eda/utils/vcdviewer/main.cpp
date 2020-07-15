477fd7d1f4602c9504cc026ac85023cdb0f69155
Michael T. Mayers <michael@tweakoz.com>
2020-07-14 17:47:13 -0700

    ..

--- ork.eda/utils/vcdviewer/main.cpp
+++ ork.eda/utils/vcdviewer/main.cpp
@@ -5,6 +5,7 @@
 #include <ork/lev2/ui/viewport.h>
 #include <ork/lev2/ui/layoutgroup.inl>
 #include <ork/lev2/ui/context.h>
+#include <ork/lev2/ui/label.h>
 #include <queue>
 #include "harness.h"
 
@@ -20,6 +21,15 @@ void TestViewport::DoDraw(ui::drawevent_constptr_t drwev) {
 }
 void TestViewport::onUpdateThreadTick(ui::updatedata_ptr_t updata) {
 }
+struct SignalTrack {
+  std::string _name;
+  signal_ptr_t _signal;
+};
+struct ScopeTrack {
+  std::string _name;
+  scope_ptr_t _scope;
+  std::vector<SignalTrack> _sigtracks;
+};
 
 int main(int argc, char** argv) {
   //////////////////////////////////////
@@ -36,20 +46,34 @@ int main(int argc, char** argv) {
   auto app = createEZapp(argc, argv);
   //////////////////////////////////////
   auto vp       = app->_topLayoutGroup;
-  auto w_header = vp->makeChild<EvTestBox>("w_header", fvec4(1, 1, 0, 1));
+  auto w_tracks = vp->makeChild<LayoutGroup>("w_tracks");
   auto w_status = vp->makeChild<EvTestBox>("status", fvec4(0, 1, 0, 1));
   //////////////////////////////////////
   auto root_layout = vp->_layout;
-  auto l_header    = w_header._layout;
+  auto l_tracks    = w_tracks._layout;
   auto l_status    = w_status._layout;
   //////////////////////////////////////
-  l_header->setMargin(4);
+  l_tracks->setMargin(4);
   l_status->setMargin(2);
+  //////////////////////////////////////
+  auto cg1 = root_layout->fixedHorizontalGuide(-32); // 1
+  //////////////////////////////////////
+  l_tracks->top()->anchorTo(root_layout->top());     // 2,3
+  l_tracks->left()->anchorTo(root_layout->left());   // 4,5
+  l_tracks->bottom()->anchorTo(cg1);                 // 6
+  l_tracks->right()->anchorTo(root_layout->right()); // 7,8
+  //////////////////////////////////////
+  l_status->top()->anchorTo(cg1);                      // 18
+  l_status->left()->anchorTo(root_layout->left());     // 19
+  l_status->bottom()->anchorTo(root_layout->bottom()); // 20,21
+  l_status->right()->anchorTo(root_layout->right());   // 22
   //////////////////////////////////////
   // recursive descent into scoped-signals
   //////////////////////////////////////
   std::stack<scope_ptr_t> scope_stack;
   std::stack<std::string> scopename_stack;
+  std::vector<ScopeTrack> scopetracks;
+  std::vector<SignalTrack> flat_sigtracks;
   scope_stack.push(vcdfile._root);
   scopename_stack.push("");
   while (not scope_stack.empty()) {
@@ -58,6 +82,18 @@ int main(int argc, char** argv) {
 
     printf("visiting scope<%s>\n", topname.c_str());
 
+    ScopeTrack scopetrk;
+    scopetrk._name  = topname;
+    scopetrk._scope = front;
+
+    for (auto sigitem : front->_signals) {
+      SignalTrack sigtrk;
+      sigtrk._name   = sigitem.second->_longname;
+      sigtrk._signal = sigitem.second;
+      scopetrk._sigtracks.push_back(sigtrk);
+      flat_sigtracks.push_back(sigtrk);
+    }
+    scopetracks.push_back(scopetrk);
     scope_stack.pop();
     scopename_stack.pop();
     for (auto ch : front->_child_scopes) {
@@ -67,17 +103,24 @@ int main(int argc, char** argv) {
     }
   }
   //////////////////////////////////////
-  auto cg1 = root_layout->fixedHorizontalGuide(-32); // 1
-  //////////////////////////////////////
-  l_header->top()->anchorTo(root_layout->top());     // 2,3
-  l_header->left()->anchorTo(root_layout->left());   // 4,5
-  l_header->bottom()->anchorTo(cg1);                 // 6
-  l_header->right()->anchorTo(root_layout->right()); // 7,8
-  //////////////////////////////////////
-  l_status->top()->anchorTo(cg1);                      // 18
-  l_status->left()->anchorTo(root_layout->left());     // 19
-  l_status->bottom()->anchorTo(root_layout->bottom()); // 20,21
-  l_status->right()->anchorTo(root_layout->right());   // 22
+  int numsigtracks = flat_sigtracks.size();
+  std::vector<anchor::guide_ptr_t> track_guides;
+  track_guides.resize(numsigtracks + 1);
+  track_guides[0] = l_tracks->top();
+  for (int i = 0; i < numsigtracks; i++) {
+    float fitop         = float(i) / float(numsigtracks);
+    float fibot         = float(i + 1) / float(numsigtracks);
+    auto gtop           = l_tracks->proportionalHorizontalGuide(fitop);
+    auto gbot           = l_tracks->proportionalHorizontalGuide(fibot);
+    track_guides[i + 1] = gbot;
+    auto sigtrack       = flat_sigtracks[i];
+    auto w              = w_tracks._widget->makeChild<Label>(sigtrack._name, fvec4(0), sigtrack._name);
+    auto wl             = w._layout;
+    wl->top()->anchorTo(gtop);
+    wl->bottom()->anchorTo(gbot);
+    wl->left()->anchorTo(l_tracks->left());
+    wl->right()->anchorTo(l_tracks->right());
+  }
   //////////////////////////////////////
   // root_layout->dump();
   // exit(0);
