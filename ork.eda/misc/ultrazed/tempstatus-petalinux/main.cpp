6608c9be3d5e42a40e2864c2872f3c62084631fc
tweakoz <michael@tweakoz.com>
2020-07-19 14:00:10 -0700

    ..

--- ork.eda/misc/ultrazed/tempstatus-petalinux/main.cpp
+++ ork.eda/misc/ultrazed/tempstatus-petalinux/main.cpp
@@ -8,30 +8,34 @@ using namespace std;
 
 int main(int argc, char** argv) {
 
-  std::string LPDAPU = "/sys/bus/iio/devices/iio:device0/in_temp0_ps_temp_raw";
-  std::string FPDRPU = "/sys/bus/iio/devices/iio:device0/in_temp1_remote_temp_raw";
-  std::string PL     = "/sys/bus/iio/devices/iio:device0/in_temp2_pl_temp_raw";
+  // xilinx-ams device path
+  // https://xilinx-wiki.atlassian.net/wiki/spaces/A/pages/18842163/Zynq+UltraScale+MPSoC+AMS
 
-  auto getv = [](std::string fname) -> int {
+  std::string XILINXAMSBASE = "/sys/bus/iio/devices/iio:device0/";
+
+  auto getv = [](std::string fname) -> double {
     ifstream the_file(fname);
     assert(the_file.is_open());
     string line;
     bool ok = (bool)getline(the_file, line);
+    // printf("line<%s>\n", line.c_str());
     assert(ok);
     the_file.close();
-    return atoi(line.c_str());
+    return atof(line.c_str());
   };
 
-  int lpdval = getv(LPDAPU);
-  int fpdval = getv(FPDRPU);
-  int plval  = getv(PL);
+  auto do_item = [&](string senspath, string desc) {
+    double raw     = getv(XILINXAMSBASE + senspath + "_raw");
+    double scale   = getv(XILINXAMSBASE + senspath + "_scale");
+    double offset  = getv(XILINXAMSBASE + senspath + "_offset");
+    double celcius = scale * (raw + offset) * 0.001;
+    double farenh  = 32.0 + celcius * 9.0 / 5.0;
+    printf("desc<%s> celcius<%g> fahrenheit<%g>\n", desc.c_str(), celcius, farenh);
+  };
 
-  double lpd_temp_celcius = (double(lpdval) * 509.314 / 65536.0) - 280.23;
-  double fpd_temp_celcius = (double(fpdval) * 509.314 / 65536.0) - 280.23;
-  double pl_temp_celcius  = (double(plval) * 509.314 / 65536.0) - 280.23;
+  do_item("in_temp0_ps_temp", "Temp_FPD(APU)");
+  do_item("in_temp1_remote_temp", "Temp_LPD(RPU)");
+  do_item("in_temp2_pl_temp", "Temp_PL(PL)");
 
-  printf("lpdval<%d> lpd_temp_celcius<%g>\n", lpdval, lpd_temp_celcius);
-  printf("fpdval<%d> fpd_temp_celcius<%g>\n", fpdval, fpd_temp_celcius);
-  printf("plval<%d> pl_temp_celcius<%g>\n", plval, pl_temp_celcius);
   return 0;
 }
