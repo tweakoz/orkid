da6fab754f26cf6f907fc55da8b151e9ec02e511
Michael T. Mayers <michael@tweakoz.com>
2020-07-23 18:50:38 -0700

    ..

--- ork.eda/misc/ultrazed/vivadotest/genclock.py
+++ ork.eda/misc/ultrazed/vivadotest/genclock.py
@@ -6,26 +6,22 @@ from ork import vivado, path, pathtools
 this_dir = path.Path(os.path.dirname(os.path.realpath(__file__)))
 pathtools.chdir(this_dir)
 
-print(os.getcwd())
+########################################################
 
-IPID = "clk_wiz"
-VENDOR = "xilinx.com"
-VERSION = "6.0"
-LIBRARY = "ip"
-PARTNAME = "xczu7ev-fbvb900-1-e"
-INSTANCENAME = "systemclocks"
-INPFREQ = 300.000 # Mhz
-OUTFREQ = 400.000 # Mhz
-INP_PERIOD = round(1000.0 / INPFREQ,3);
+INP_FREQ = 300.000 # Mhz
+OUT_FREQ = 400.000 # Mhz
+INP_PERIOD = round(1000.0 / INP_FREQ,3);
 OUT_PHASE_A = 0.000 # Degrees
 OUT_PHASE_B = 180.000 # Degrees
 
+########################################################
+
 MMCMDICT = {
   "CONFIG.NUM_OUT_CLKS": 2,
-  "CONFIG.CLKOUT2_USED": True, # CONFIG.CLKOUT2_USED {true}
+  "CONFIG.CLKOUT2_USED": True,
 
-  "CONFIG.USE_MIN_POWER": True, # CONFIG.USE_MIN_POWER {true}
-  "CONFIG.PRIM_IN_FREQ": INPFREQ, # CONFIG.PRIM_IN_FREQ {$INPFREQ}
+  "CONFIG.USE_MIN_POWER": True,
+  "CONFIG.PRIM_IN_FREQ": INP_FREQ,
   "CONFIG.USE_SAFE_CLOCK_STARTUP": True,
   "CONFIG.JITTER_SEL": "No_Jitter",
   "CONFIG.FEEDBACK_SOURCE": "FDBK_AUTO",
@@ -42,38 +38,19 @@ MMCMDICT = {
 
   "CONFIG.CLKIN1_JITTER_PS": 33.330000000000005,
 
-  "CONFIG.CLKOUT1_REQUESTED_OUT_FREQ": OUTFREQ,
+  "CONFIG.CLKOUT1_REQUESTED_OUT_FREQ": OUT_FREQ,
   "CONFIG.CLKOUT1_REQUESTED_PHASE": OUT_PHASE_A,
   "CONFIG.CLKOUT1_DRIVES": "BUFGCE",
 
-  "CONFIG.CLKOUT2_REQUESTED_OUT_FREQ": OUTFREQ,
+  "CONFIG.CLKOUT2_REQUESTED_OUT_FREQ": OUT_FREQ,
   "CONFIG.CLKOUT2_REQUESTED_PHASE": OUT_PHASE_B,
   "CONFIG.CLKOUT2_DRIVES": "BUFGCE",
-
- # "CONFIG.MMCM_CLKFBOUT_MULT_F": 8.000,
-
- # "CONFIG.MMCM_CLKOUT0_DIVIDE_F": 2.000,
-
-  #"CONFIG.CLKOUT1_JITTER": 109.830,
-  #"CONFIG.CLKOUT1_PHASE_ERROR": 114.212,
-
-  #"CONFIG.CLKOUT2_JITTER": 109.830,
-  #"CONFIG.CLKOUT2_PHASE_ERROR": 114.212,
-
-  #"CONFIG.CLKOUT3_DRIVES": "BUFGCE",
-  #"CONFIG.CLKOUT4_DRIVES": "BUFGCE",
-  #"CONFIG.CLKOUT5_DRIVES": "BUFGCE",
-  #"CONFIG.CLKOUT6_DRIVES": "BUFGCE",
-  #"CONFIG.CLKOUT7_DRIVES": "BUFGCE",
-
-  #"CONFIG.MMCM_CLKOUT1_PHASE": OUT_PHASE_B,
-  #"CONFIG.MMCM_CLKOUT1_PHASE": OUT_PHASE_B,
 }
 
-IPPROPERTIES = ''.join(['%s {%s} ' % (key, str(value)) for (key, value) in MMCMDICT.items()])
-IPPROPERTIES = IPPROPERTIES.replace("True","true")
-IPPROPERTIES = IPPROPERTIES.replace("False","false")
-print(IPPROPERTIES)
+########################################################
+########################################################
+
+INSTANCENAME = "systemclocks"
 
 os.system("rm -rf %s"%(this_dir/".gen"/INSTANCENAME))
 os.system("rm -rf %s"%(this_dir/".ip_user_files"))
@@ -86,10 +63,10 @@ vivado.genIP(dirmaps=dirmaps,
              vivworkingdir="/tmp/build",
              tclhostfilename=this_dir/".gen"/"genclock.tcl",
              tclcontfilename=".gen/genclock.tcl",
-             IPID=IPID,
-             VENDOR=VENDOR,
-             VERSION=VERSION,
-             LIBRARY=LIBRARY,
-             PARTNAME=PARTNAME,
+             IPID="clk_wiz",
+             VENDOR="xilinx.com",
+             VERSION="6.0",
+             LIBRARY="ip",
+             PARTNAME="xczu7ev-fbvb900-1-e",
              INSTANCENAME=INSTANCENAME,
-             IPPROPERTIES=IPPROPERTIES)
+             IPPROPERTIES=MMCMDICT)
