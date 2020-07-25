c4759291d1ed3981144daee84ec783808c9f3d71
Michael T. Mayers <michael@tweakoz.com>
2020-07-24 18:20:17 -0700

    ..

--- ork.eda/misc/ultrazed/vivadotest/build.py
+++ ork.eda/misc/ultrazed/vivadotest/build.py
@@ -1,23 +1,43 @@
 #!/usr/bin/env python3
 
-import os
-from ork import vivado, path, pathtools
+import os, argparse, sys
+from ork import path, pathtools
 
-#todo invoke genclock.py
+parser = argparse.ArgumentParser(description='vivadotest build')
+parser.add_argument('--clean', action="store_true", help='cleanup transient data' )
+parser.add_argument('--genip', action="store_true", help='generate initial IP' )
+
+_args = vars(parser.parse_args())
 
 this_dir = path.Path(os.path.dirname(os.path.realpath(__file__)))
 pathtools.chdir(this_dir)
 
-print(os.getcwd())
+if _args["clean"]:
+  os.system("rm -rf %s"%(this_dir/".build"))
+  os.system("rm -rf %s"%(this_dir/".gen"))
+  _args["genip"]=True
 
-dirmaps = {
-  this_dir: "/tmp/build"
-}
-vivado.run(dirmaps=dirmaps,
-           workingdir="/tmp/build",
-           args=["-mode",
-            "batch",
-            "-nojournal",
-            "-nolog",
-            "-source",
-            "cleanbuild.tcl"])
+########################################################
+########################################################
+from ork.eda.xilinx import vivado, mmcm
+########################################################
+########################################################
+vivctx = vivado.Context(hostdir=this_dir,
+                        FPGAPART="xczu7ev-fbvb900-1-e")
+########################################################
+# Generate required procedural IP
+#  systemclocks, etc..
+########################################################
+if _args["genip"]:
+  myclocks = [mmcm.OutClock(350.000,0.000),
+              mmcm.OutClock(350.000,180.000)]
+  mmcm.generate(vivctx,
+                INP_NAME="SYSCLK_P",
+                INSTANCENAME="systemclocks",
+                differential=True,
+                INP_FREQ = 300.000, # mhz,
+                outclocks = myclocks)
+########################################################
+# main build
+########################################################
+vivctx.run_tclscript("cleanbuild.tcl")
