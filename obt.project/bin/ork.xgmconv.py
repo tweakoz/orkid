b360fe39127d4348b66cda0c0d8533b74ac5ab39
Michael Mayers <michael@tweakoz.com>
2022-06-20 19:47:50 -0600

    ..

--- obt.project/bin/ork.xgmconv.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import os, sys,string
-
-args = string.join(sys.argv[1:])
-cmd = "$TOZ_STAGE/bundle/OrkidTool.app/Contents/MacOS/ork.tool.test.osx.release -filter dae:xgm " + args
-print cmd
-os.system( cmd )
--- obt.project/scripts/init_env.py
+++ obt.project/scripts/init_env.py
@@ -1,9 +1,21 @@
+###############################################################################
+# Orkid Media Engine
+# Copyright 2010-2022, Michael T. Mayers
+# email: michael@tweakoz.com
+###############################################################################
+# orkid as project initialization
+
 import os
 import ork.deco
 import ork.env
 import ork.path
 
 def setup():
+
+  ##############################################
+  # compute paths
+  ##############################################
+
   deco = ork.deco.Deco()
   file_path = os.path.realpath(__file__)
   scripts_dir = ork.path.Path(os.path.dirname(file_path))
@@ -12,10 +24,25 @@ def setup():
   orkbin_dir = obtprj_dir/"bin"
   ORK_PROJECT_NAME = "Orkid"
   assert(orkid_dir.exists())
+
+  ##############################################
+  # mark ORKID as project
+  ##############################################
+
   ork.env.set("ORKID_WORKSPACE_DIR",orkid_dir)
+  ork.env.set("ORKID_IS_MAIN_PROJECT","1")
+
+  ##############################################
+  # add orkid scripts to enviromment PATH
+  ##############################################
+
   ork.env.prepend("PATH",orkbin_dir)
 
-  #ork.env.append("OBT_SEARCH_PATH",orkid_dir/"ork.build")
+  ##############################################
+  # add search paths
+  #  for obt.find.py
+  ##############################################
+
   ork.env.append("OBT_SEARCH_PATH",orkid_dir/"obt.project")
   ork.env.append("OBT_SEARCH_PATH",orkid_dir/"ork.dox")
   ork.env.append("OBT_SEARCH_PATH",orkid_dir/"ork.data")
@@ -27,9 +54,16 @@ def setup():
   ork.env.append("OBT_SEARCH_PATH",orkid_dir/"ork.tuio")
   ork.env.append("LUA_PATH",orkid_dir/"ork.data"/"src"/"scripts"/"?.lua")
 
+  ##############################################
+  # add search extensions
+  #  for obt.find.py
+  ##############################################
+
   ork.env.append("OBT_SEARCH_EXTLIST", ".cpp:.c:.cc:.h:.hpp:.inl")
   ork.env.append("OBT_SEARCH_EXTLIST", ".qml:.m:.mm:.py")
   ork.env.append("OBT_SEARCH_EXTLIST", ".txt:.md:.glfx:.ini")
+
+  ##############################################
 
   print(deco.yellow("Initialized Orkid Build Enviroment"))
 
