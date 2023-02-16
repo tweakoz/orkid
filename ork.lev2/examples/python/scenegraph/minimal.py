31faec5d9a72cf89ad695816caae044723da1af4
Michael Mayers <michael@tweakoz.com>
2023-02-16 15:30:11 -0700

    ..

--- ork.lev2/examples/python/scenegraph/minimal.py
+++ ork.lev2/examples/python/scenegraph/minimal.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+
 ################################################################################
 # lev2 sample which renders a scenegraph to a window
 # Copyright 1996-2020, Michael T. Mayers.
 # Distributed under the Boost Software License - Version 1.0 - August 17, 2003
 # see http://www.boost.org/LICENSE_1_0.txt
 ################################################################################
+
 import math, sys, os
 from pathlib import Path
 from orkengine.core import *
@@ -13,52 +15,55 @@ sys.path.append((thisdir()/"..").normalized.as_string) # add parent dir to path
 from common.cameras import *
 from common.shaders import *
 from common.primitives import createFrustumPrim
-#########################################
+
 tokens = CrcStringProxy()
+
 ################################################################################
-class PyOrkApp(object):
-  ################################################
+
+class MinimalSceneGraphApp(object):
+
   def __init__(self):
     super().__init__()
     self.ezapp = OrkEzApp.create(self)
     self.ezapp.setRefreshPolicy(RefreshFastest, 0)
     self.materials = set()
+    setupUiCamera( app=self )
+
   ################################################
   # gpu data init:
   #  called on main thread when graphics context is
   #   made available
   ##############################################
+
   def onGpuInit(self,ctx):
-    FBI = ctx.FBI() # framebuffer interface
+
+    ###################################
+    # create scenegraph
+    ###################################
+
+    sceneparams = VarMap()
+    sceneparams.preset = "DeferredPBR"
+    self.scene = self.ezapp.createScene(sceneparams)
+    layer = self.scene.createLayer("layer1")
+
+    ###################################
+    # create frustum primitive / sgnode
+    ###################################
+
+    vmatrix = ctx.lookAt( vec3(0,0,-1), # eye
+                          vec3(0,0,0),  # tgt
+                          vec3(0,1,0) ) # up
+    pmatrix = ctx.perspective(45,1,0.1,3)
+
+    frustum_prim = createFrustumPrim(ctx=ctx,vmatrix=vmatrix,pmatrix=pmatrix)
 
     pipeline = createPipeline( app = self,
                                ctx = ctx,
                                techname = "std_mono",
                                rendermodel = "DeferredPBR" )
 
-    ###################################
-    # frustum primitive
-    ###################################
-    vmatrix = ctx.lookAt( vec3(0,0,-1), # eye
-                          vec3(0,0,0),  # tgt
-                          vec3(0,1,0) ) # up
-    pmatrix = ctx.perspective(45,1,0.1,3)
-    frustum_prim = createFrustumPrim(ctx=ctx,vmatrix=vmatrix,pmatrix=pmatrix,alpha=0.35)
-    ###################################
-    # create scenegraph and sg node
-    ###################################
-    sceneparams = VarMap()
-    sceneparams.preset = "DeferredPBR"
-    self.scene = self.ezapp.createScene(sceneparams)
-    layer = self.scene.createLayer("layer1")
     self.primnode = frustum_prim.createNode("node1",layer,pipeline)
-    ###################################
-    # create camera
-    ###################################
-    self.camera = CameraData()
-    self.camera.perspective(0.1, 100.0, 45.0)
-    self.cameralut = CameraDataLut()
-    self.cameralut.addCamera("spawncam",self.camera)
+
   ################################################
   # update:
   # technically this runs from the orkid update thread
@@ -69,6 +74,7 @@ class PyOrkApp(object):
   #   (eg. the scene is updated from python, whilst
   #        concurrently c++ is rendering..)
   ################################################
+
   def onUpdate(self,updinfo):
     θ    = updinfo.absolutetime * math.pi * 2.0 * 0.1
     ###################################
@@ -84,6 +90,7 @@ class PyOrkApp(object):
     xf.scale = 1+(1+math.sin(updinfo.absolutetime*2))
     ###################################
     self.scene.updateScene(self.cameralut) # update and enqueue all scenenodes
-  ############################################
-app = PyOrkApp()
-app.ezapp.mainThreadLoop()
+
+###############################################################################
+
+MinimalSceneGraphApp().ezapp.mainThreadLoop()
