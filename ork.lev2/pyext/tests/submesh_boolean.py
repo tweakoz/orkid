49cb56fca52baf75cc0c2df48dae73eb09a281f4
Michael T. Mayers <michael@tweakoz.com>
2023-03-26 01:16:04 -0600

    ..

--- ork.lev2/pyext/tests/submesh_boolean.py
+++ ork.lev2/pyext/tests/submesh_boolean.py
@@ -5,19 +5,10 @@
 # see http://www.boost.org/LICENSE_1_0.txt
 ################################################################################
 import ork.path
-from orkengine.core import *
-from orkengine.lev2 import *
-import trimesh, pickle
-
-################################################################################
-# Copyright 1996-2020, Michael T. Mayers.
-# Distributed under the Boost Software License - Version 1.0 - August 17, 2003
-# see http://www.boost.org/LICENSE_1_0.txt
-################################################################################
-
 import math, random, argparse, sys
 from orkengine.core import *
 from orkengine.lev2 import *
+import trimesh, pickle
 
 ################################################################################
 
