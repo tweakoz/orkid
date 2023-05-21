c11a37fd77676161e3c958f6f178da4e39457a28
Michael Mayers <michael@tweakoz.com>
2023-05-21 12:52:47 -0600

    ..

--- ork.lev2/src/lev2_enums.cpp
+++ ork.lev2/src/lev2_enums.cpp
@@ -12,11 +12,14 @@
 #include <ork/reflect/enum_serializer.inl>
 #include <ork/reflect/properties/DirectEnum.inl>
 
+/////////////////////////////////////////////////////////////////////////////////////////////////..
 
 using msaaenum_t = ork::lev2::MsaaSamples;
+using blending_t = ork::lev2::Blending;
+
+/////////////////////////////////////////////////////////////////////////////////////////////////..
 
 DeclareEnumSerializer(msaaenum_t);
-
 BeginEnumRegistration(msaaenum_t);
     RegisterEnum(msaaenum_t, MSAA_1X);
     RegisterEnum(msaaenum_t, MSAA_4X);
@@ -26,14 +29,31 @@ BeginEnumRegistration(msaaenum_t);
     RegisterEnum(msaaenum_t, MSAA_25X);
     RegisterEnum(msaaenum_t, MSAA_36X);
 EndEnumRegistration();
-
 ImplementEnumSerializer(msaaenum_t);
+template<> class ork::reflect::DirectEnum<msaaenum_t>;
+
+/////////////////////////////////////////////////////////////////////////////////////////////////..
+
+DeclareEnumSerializer(blending_t);
+BeginEnumRegistration(blending_t);
+    RegisterEnum(blending_t, OFF);
+    RegisterEnum(blending_t, PREMA);
+    RegisterEnum(blending_t, ALPHA);
+    RegisterEnum(blending_t, DSTALPHA);
+    RegisterEnum(blending_t, ADDITIVE);
+    RegisterEnum(blending_t, ALPHA_ADDITIVE);
+    RegisterEnum(blending_t, SUBTRACTIVE);
+    RegisterEnum(blending_t, ALPHA_SUBTRACTIVE);
+    RegisterEnum(blending_t, MODULATE);
+EndEnumRegistration();
+ImplementEnumSerializer(blending_t);
+template<> class ork::reflect::DirectEnum<blending_t>;
+
+/////////////////////////////////////////////////////////////////////////////////////////////////..
 
 namespace ork::lev2{
-
     void registerEnums(){
         InvokeEnumRegistration(msaaenum_t);
+        InvokeEnumRegistration(blending_t);
     }
 }
-
-template<> class ork::reflect::DirectEnum<ork::lev2::MsaaSamples>;
