bc542b0619019b8902ccf44886423a1bce425eb2
Michael Mayers <michael@tweakoz.com>
2023-11-08 13:04:48 -0700

    cleanup

--- ork.lev2/pyext/tests/renderer/deferred_projmap.py
+++ ork.lev2/pyext/tests/renderer/deferred_projmap.py
@@ -24,8 +24,174 @@ from common.scenegraph import createSceneGraph
 
 ################################################################################
 
+def defaultSceneGraphParams():
+  sg_params_def = VarMap()
+  sg_params_def.SkyboxIntensity = 3.0
+  sg_params_def.DiffuseIntensity = 1.0
+  sg_params_def.SpecularIntensity = 1.0
+  sg_params_def.AmbientLevel = vec3(.125)
+  return sg_params_def
+
+################################################################################
+
 save_images = False 
 do_offscreen = False 
+
+class Panel:
+
+  def __init__(self,parent,index):
+    #
+    self.parent = parent
+    self.index = index
+    self.camname = "Camera%d"%index
+    self.use_event = False
+
+    #
+
+    self.cameralut = CameraDataLut()
+    self.cur_eye = vec3(0,0,0)
+    self.cur_tgt = vec3(0,0,1)
+    self.dst_eye = vec3(0,0,0)
+    self.dst_tgt = vec3(0,0,0)
+    self.counter = 0
+                
+
+    griditem = parent.griditems[index]        
+    self.cameralut = CameraDataLut()
+    self.camera, self.uicam = setupUiCameraX( cameralut=self.cameralut, 
+                                              near = 0.01,
+                                              far = 100.0,
+                                              eye = vec3(0,5,20),
+                                              tgt = vec3(0,5,0),
+                                              up = vec3(0,1,0),
+                                              camname=self.camname )
+
+    #################################################
+    # scenegraph 0 gets standard deferred pbr
+    #################################################
+
+    if index==0:
+      sg_params_def = defaultSceneGraphParams()
+      sg_params_def.preset = "DeferredPBR"
+      self.scenegraph = scenegraph.Scene(sg_params_def)
+
+    #################################################
+    # scenegraph 1 gets deferred pbr node with 
+    #  custom environmentlighting shader 
+    #################################################
+
+    else:
+      comp_tek = NodeCompositingTechnique()
+      comp_tek.renderNode = DeferredPbrRenderNode()
+      comp_tek.outputNode = ScreenOutputNode()
+
+      comp_data = CompositingData()
+      comp_scene = comp_data.createScene("scene1")
+      comp_sceneitem = comp_scene.createSceneItem("item1")
+      comp_sceneitem.technique = comp_tek
+
+      # OVERRIDES
+
+      pbr_common = comp_tek.renderNode.pbr_common
+      pbr_common.requestSkyboxTexture("src://envmaps/tozenv_hellscape")
+      comp_tek.renderNode.overrideShader(str(this_dir/"deferred_projmap.glfx"))
+      sg_params_def = defaultSceneGraphParams()
+      sg_params_def.preset = "USER"
+      sg_params_def.compositordata = comp_data
+      self.scenegraph = scenegraph.Scene(sg_params_def)
+      self.comp_tek = comp_tek
+
+    #################################################
+    # cache output and render nodes for scenegraph
+    #################################################
+
+    self.output_node = self.scenegraph.compositoroutputnode
+    self.render_node = self.scenegraph.compositorrendernode
+
+    #################################################
+    # scenegraph 1 gets custom shader parameter bindings
+    #  to feed custom environmentlighting shader 
+    #################################################
+
+    if index==1:
+      self.deferred_ctx = self.render_node.context
+      self.depthtex_binding = self.deferred_ctx.createAuxBinding("MapShadowDepth")
+      self.projtex_binding = self.deferred_ctx.createAuxBinding("ProjectionTexture")
+      self.projmtx_binding = self.deferred_ctx.createAuxBinding("ProjectionTextureMatrix")
+      self.projcam_eye = self.deferred_ctx.createAuxBinding("ProjectionEyePostion")
+      self.nearfar_binding = self.deferred_ctx.createAuxBinding("NearFar")
+
+    #################################################
+    # both scenes will render the same content
+    #  but with different cameras and environment lighting
+    #################################################
+
+    self.use_event = True
+    self.layer = self.scenegraph.createLayer("layer")
+    self.sgnode = parent.model.createNode("modelnode",self.layer)
+
+    self.anim_inst = XgmAnimInst(parent.anim)
+    self.anim_inst.mask.enableAll()
+    self.anim_inst.use_temporal_lerp = True
+    self.anim_inst.bindToSkeleton(parent.model.skeleton)
+
+    self.modelinst = self.sgnode.user.pyext_retain_modelinst
+    self.modelinst.enableSkinning()
+    self.modelinst.enableAllMeshes()
+    self.localpose = self.modelinst.localpose
+    self.worldpose = self.modelinst.worldpose
+    griditem.widget.cameraName = self.camname
+    griditem.widget.scenegraph = self.scenegraph
+
+    #################################################
+    # gpu udpate per panel
+    #################################################
+
+    def gpu_update_handler(context):
+
+      # update skeleton pose from animation
+
+      self.localpose.bindPose()
+      self.anim_inst.currentFrame = parent.abstime*30.0
+      self.anim_inst.weight = 1.0
+      self.anim_inst.applyToPose(self.localpose)
+      self.localpose.blendPoses()
+      self.localpose.concatenate()
+      self.worldpose.fromLocalPose(self.localpose,mtx4())
+
+    # register update handler
+
+    parent.gpu_update_handlers += [gpu_update_handler]
+
+    #################################################
+    # fork rendering database per panel
+    #################################################
+
+    griditem.widget.forkDB()
+
+    ########################################### 
+    # route events to panels ui camera ?
+    ########################################### 
+
+    def onPanelEvent(index, event):
+      if event.code == tokens.KEY_DOWN.hashed or event.code == tokens.KEY_REPEAT.hashed:
+        if event.keycode == 32: # spacebar
+          self.use_event = not self.use_event
+
+      if self.use_event:
+        self.uicam.uiEventHandler(event)
+      
+      return ui.HandlerResult()
+
+    griditem.widget.evhandler = lambda ev: onPanelEvent(index,ev)
+
+  ###
+
+  def update(self):
+    self.uicam.updateMatrices()
+    self.camera.copyFrom( self.uicam.cameradata )
+    self.scenegraph.updateScene(self.cameralut)
+
 
 ################################################################################
 
@@ -66,224 +232,16 @@ class UiSgQuadViewTestApp(object):
     self.cameralut = self.ezapp.vars.cameras
     self.uicontext = self.ezapp.uicontext
 
-
     ########################################################
     # shared geometry
     ########################################################
     
-    self.grid_data = createGridData()
-    cube_prim = createCubePrim(ctx=ctx,size=2.0)
-    pipeline_cube = createPipeline( app = self, ctx = ctx, rendermodel="FORWARD_PBR", techname="std_mono_fwd" )
-    mesh = meshutil.Mesh()
-    mesh.readFromWavefrontObj("data://tests/simple_obj/cone.obj")
-    submesh = mesh.submesh_list[0]
-    submesh_prim = meshutil.RigidPrimitive(submesh,ctx)
-    pipeline_mesh = createPipeline( app = self, ctx = ctx, rendermodel="FORWARD_PBR", techname="std_mono_fwd" )
-
     self.model = XgmModel("data://tests/chartest/char_mesh")
     self.anim = XgmAnim("data://tests/chartest/char_testanim1")
 
     ########################################################
-    # scenegraph init data
-    ########################################################
-
-    sg_params_fwd = VarMap()
-    sg_params_fwd.SkyboxIntensity = 3.0
-    sg_params_fwd.DiffuseIntensity = 1.0
-    sg_params_fwd.SpecularIntensity = 1.0
-    sg_params_fwd.AmbientLevel = vec3(.125)
-    sg_params_fwd.preset = "ForwardPBR"
-
-
-    ########################################################
     # create scenegraph / panels
     ########################################################
-
-    class Panel:
-
-      ####################################################################################
-
-      def __init__(self,parent,index):
-        #
-        self.parent = parent
-        self.index = index
-        self.camname = "Camera%d"%index
-        self.use_event = False
-
-        #
-
-        self.cameralut = CameraDataLut()
-        self.cur_eye = vec3(0,0,0)
-        self.cur_tgt = vec3(0,0,1)
-        self.dst_eye = vec3(0,0,0)
-        self.dst_tgt = vec3(0,0,0)
-        self.counter = 0
-
-                    
-
-        griditem = parent.griditems[index]        
-        self.cameralut = CameraDataLut()
-        self.camera, self.uicam = setupUiCameraX( cameralut=self.cameralut, 
-                                                  near = 0.01,
-                                                  far = 100.0,
-                                                  eye = vec3(0,5,20),
-                                                  tgt = vec3(0,5,0),
-                                                  up = vec3(0,1,0),
-                                                  camname=self.camname )
-        if True:
-
-          if index==0:
-            sg_params_def = VarMap()
-            sg_params_def.SkyboxIntensity = 3.0
-            sg_params_def.DiffuseIntensity = 1.0
-            sg_params_def.SpecularIntensity = 1.0
-            sg_params_def.AmbientLevel = vec3(.125)
-            sg_params_def.preset = "DeferredPBR"
-            self.scenegraph = scenegraph.Scene(sg_params_def)
-          else:
-            comp_tek = NodeCompositingTechnique()
-            comp_tek.renderNode = DeferredPbrRenderNode()
-            comp_tek.outputNode = ScreenOutputNode()
-
-            comp_data = CompositingData()
-            comp_scene = comp_data.createScene("scene1")
-            comp_sceneitem = comp_scene.createSceneItem("item1")
-            comp_sceneitem.technique = comp_tek
-
-            # OVERRIDES
-
-            pbr_common = comp_tek.renderNode.pbr_common
-            pbr_common.requestSkyboxTexture("src://envmaps/tozenv_hellscape")
-            pbr_common.environmentIntensity = 1
-            pbr_common.environmentMipBias = 10
-            pbr_common.environmentMipScale = 1
-            pbr_common.diffuseLevel = 1
-            pbr_common.specularLevel = 1
-            pbr_common.specularMipBias = 1
-            pbr_common.skyboxLevel = .5
-            pbr_common.depthFogDistance = 100
-            pbr_common.depthFogPower = 1
-            comp_tek.renderNode.overrideShader(str(this_dir/"deferred_projmap.glfx"))
-
-            print(comp_sceneitem)
-            print(comp_tek)
-            print(pbr_common)
-            sg_params_xxx = VarMap()
-            sg_params_xxx.preset = "USER"
-            sg_params_xxx.compositordata = comp_data
-            self.scenegraph = scenegraph.Scene(sg_params_xxx)
-            self.comp_tek = comp_tek
-
-          self.output_node = self.scenegraph.compositoroutputnode
-          self.render_node = self.scenegraph.compositorrendernode
-
-          if index==1:
-            self.deferred_ctx = self.render_node.context
-            self.depthtex_binding = self.deferred_ctx.createAuxBinding("MapShadowDepth")
-            self.projtex_binding = self.deferred_ctx.createAuxBinding("ProjectionTexture")
-            self.projmtx_binding = self.deferred_ctx.createAuxBinding("ProjectionTextureMatrix")
-            self.projcam_eye = self.deferred_ctx.createAuxBinding("ProjectionEyePostion")
-            self.nearfar_binding = self.deferred_ctx.createAuxBinding("NearFar")
-          self.use_event = True
-          self.layer = self.scenegraph.createLayer("layer")
-          self.sgnode = parent.model.createNode("modelnode",self.layer)
-
-          self.anim_inst = XgmAnimInst(parent.anim)
-          self.anim_inst.mask.enableAll()
-          self.anim_inst.use_temporal_lerp = True
-          self.anim_inst.bindToSkeleton(parent.model.skeleton)
-
-          self.modelinst = self.sgnode.user.pyext_retain_modelinst
-          self.modelinst.enableSkinning()
-          self.modelinst.enableAllMeshes()
-          self.localpose = self.modelinst.localpose
-          self.worldpose = self.modelinst.worldpose
-          griditem.widget.cameraName = self.camname
-          griditem.widget.scenegraph = self.scenegraph
-
-          def gpu_update_handler(context):
-            self.localpose.bindPose()
-            self.anim_inst.currentFrame = parent.abstime*30.0
-            self.anim_inst.weight = 1.0
-            self.anim_inst.applyToPose(self.localpose)
-            self.localpose.blendPoses()
-            self.localpose.concatenate()
-            self.worldpose.fromLocalPose(self.localpose,mtx4())
-
-          parent.gpu_update_handlers += [gpu_update_handler]
-
-        #
-
-        griditem.widget.forkDB()
-
-        ########################################### 
-        # route events to panels ui camera ?
-        ########################################### 
-
-        def onPanelEvent(index, event):
-          if event.code == tokens.KEY_DOWN.hashed or event.code == tokens.KEY_REPEAT.hashed:
-            if event.keycode == 32: # spacebar
-              self.use_event = not self.use_event
-
-          if self.use_event:
-            self.uicam.uiEventHandler(event)
-          
-          return ui.HandlerResult()
-
-        griditem.widget.evhandler = lambda ev: onPanelEvent(index,ev)
-
-        ########################################### 
-
-        if save_images:
-          self.capbuf = CaptureBuffer()
-          self.frame_index = 0
-
-          def _on_render():
-            rtgroup = griditem.widget.rtgroup
-            rtbuffer = rtgroup.buffer(0) #rtg's MRT buffer 0
-            FBI = ctx.FBI()
-            FBI.captureAsFormat(rtbuffer,self.capbuf,"RGBA8")
-            as_np = numpy.array(self.capbuf,dtype=numpy.uint8).reshape( rtgroup.height, rtgroup.width, 4 )
-            img = Image.fromarray(as_np, 'RGBA')
-            flipped = img.transpose(Image.FLIP_TOP_BOTTOM)
-            out_path = ork.path.temp()/("%s-%003d.png"%(camname,self.frame_index))
-            flipped.save(out_path)
-            self.frame_index += 1
-
-          griditem.widget.onPostRender(_on_render)
-
-        self.griditem = griditem
-
-      ####################################################################################
-
-      def update(self):
-        def genpos():
-          r = vec3(0)
-          r.x = random.uniform(-10,10)
-          r.z = random.uniform(-10,10)
-          r.y = random.uniform(  0,10)
-          return r 
-      
-        if self.counter<=0:
-          self.counter = int(random.uniform(1,1000))
-          self.dst_eye = genpos()
-          self.dst_tgt = vec3(0,random.uniform(  0,2),0)
-
-        if not self.use_event:
-          self.cur_eye = self.cur_eye*0.9995 + self.dst_eye*0.0005
-          self.cur_tgt = self.cur_tgt*0.9995 + self.dst_tgt*0.0005
-          self.uicam.distance = 1
-          self.uicam.lookAt( self.cur_eye,
-                            self.cur_tgt,
-                            vec3(0,1,0))
-
-        self.counter = self.counter-1
-
-        self.uicam.updateMatrices()
-        self.camera.copyFrom( self.uicam.cameradata )
-        self.scenegraph.updateScene(self.cameralut)
-
-    ##########################################################################
 
     self.panels = [
       Panel(self, 0),
@@ -298,51 +256,59 @@ class UiSgQuadViewTestApp(object):
   ################################################
 
   def onGpuUpdate(self,context):
+
+    #####################
+    # invoke per panel gpu update handlers
+    #####################
+
     for handler in self.gpu_update_handlers:
       handler(context)
+
     #####################
-    p0 = self.panels[0]
-    p1 = self.panels[1]
+
+    panel_0 = self.panels[0]
+    panel_1 = self.panels[1]
+
     #####################
-    # fetch lightaccum from panel 0
+    # fetch from panel 0
+    #  1. deferred context
+    #  2. light accumulation buffer
     #####################
-    sg0 = p0.scenegraph
-    rn0 = p0.render_node
-    defctx0 = rn0.context
-    gbuffer0 = defctx0.gbuffer
-    lbuffer0 = defctx0.lbuffer
-    if gbuffer0!=None:
-      #gbuf = gbuffer0.mrt_buffer(0)
-      zbuf = gbuffer0.depth_buffer
-      self.depth_texure = zbuf.texture
+
+    scenegraph_0 = panel_0.scenegraph
+    rendernode_0 = panel_0.render_node
+    deferred_context = rendernode_0.context
+    light_accum_buffer_0 = deferred_context.lbuffer
+
+    #####################
     # Bind Auxiliary shader parameters
-    if lbuffer0!=None:
-      L = lbuffer0.mrt_buffer(0).texture
-      self.laccum_texure = L
+    #####################
+
+    if light_accum_buffer_0!=None:
       #
+      gbuffer0 = deferred_context.gbuffer
+      zbuffer0 = gbuffer0.depth_buffer
+      L = light_accum_buffer_0.mrt_buffer(0).texture
+      Z = zbuffer0.texture
+      panel_1.projtex_binding.texture = L
+      panel_1.depthtex_binding.texture = Z
+      # we need an aspect ratio to compute the vp matrix
       aspect = float(L.width)/float(L.height)
-      vp_matrix = p0.camera.vpMatrix(aspect)
+      vp_matrix = panel_0.camera.vpMatrix(aspect)
       #
-      p1.projtex_binding.texture = L
-      p1.depthtex_binding.texture = zbuf.texture
-      p1.projmtx_binding.mtx4 = vp_matrix
-      #print(vp_matrix)
-      p1.nearfar_binding.vec2 = vec2(p0.camera.near,p0.camera.far)
-      #p1.projcam_eye.vec3 = vp_matrix.inverse.translation
-      p1.projcam_eye.vec3 = p0.camera.eye
-      #print(vp_matrix.inverse.translation,p0.camera.eye)
+      panel_1.projmtx_binding.mtx4 = vp_matrix
+      panel_1.nearfar_binding.vec2 = vec2(panel_0.camera.near,panel_0.camera.far)
+      panel_1.projcam_eye.vec3 = panel_0.camera.eye
+
   ################################################
 
   def onUpdate(self,updinfo):
-
     self.abstime = updinfo.absolutetime
-
     for panel in self.panels:
       panel.update()
-
+    # in UI mode we have to mark widgets dirty so the texture suface can repaint.
     for g in self.griditems:
       g.widget.setDirty()
-    
 
 ###############################################################################
 
