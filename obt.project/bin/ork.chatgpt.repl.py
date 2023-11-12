d4c72605e0e4d0b64e7a6150b502af8327026db2
Michael T. Mayers <michael@tweakoz.com>
2023-11-12 11:55:47 -0800

    ..

--- obt.project/bin/ork.chatgpt.repl.py
+++ obt.project/bin/ork.chatgpt.repl.py
@@ -1,137 +1,159 @@
 #!/usr/bin/env python3
 #########################################
-import os 
-from openai import OpenAI
-from obt import command, path
+import os, time
+import openai
+from obt import command, path, deco
 
+deco = deco.Deco()
 #########################################
 
 API_KEY = os.environ['OPENAI_API_KEY']
-#MODEL = "gpt-4-1106-preview"
-MODEL = "gpt-4"
+MODEL = "gpt-4-1106-preview"
+#MODEL = "gpt-4"
 
+#########################################
+
+client = openai.OpenAI(api_key=API_KEY)
+print(client)
+
+#########################################
+
+def upload_files(file_paths):
+  file_ids = []
+  ####################
+  # aggregate source files
+  ####################
+  aggregated = ""
+  for file_path in file_paths:
+    path_as_str = str(file_path)
+    text = open(path_as_str, "r").read()
+    aggregated += text
+  ####################
+  # write aggregated
+  ####################
+  out_path = path.temp()/"orkid_aggregated.h"
+  open(str(out_path), "w").write(aggregated)
+  ####################
+  print(deco.yellow("uploading to context:  file<%s>"%out_path))
+  # TODO - upload file without chatgpt being able 
+  # to see the absolute path of the file
+  file = client.files.create(
+    file=open(str(out_path), "rb"),
+    purpose='assistants'
+  )
+  file_ids.append(file.id)
+  return file_ids
+
+#########################################
+# Upload your files
 #########################################
 
 core_dir = path.orkid()/"ork.core"
 lev2_dir = path.orkid()/"ork.lev2"
 lev2_inc_dir = lev2_dir/"inc"
-lev2_gfx_inc = path.Path("/ork")/"lev2"/"gfx"
+lev2_gfx_inc = lev2_inc_dir/"ork"/"lev2"/"gfx"
 core_inc_dir = core_dir/"inc"
-kern_cor_inc = path.Path("/ork")/"kernel"
+kern_cor_inc = core_inc_dir/"ork"/"kernel"
+
+file_paths  = [kern_cor_inc/"orktypes.h"] 
+file_paths  = [kern_cor_inc/"orkstd.h"] 
+file_paths  = [kern_cor_inc/"svariant.h"] 
+file_paths += [kern_cor_inc/"varmap.inl"] 
+file_paths  = [kern_cor_inc/"kernel"/"treeops.inl"] 
+file_paths  = [kern_cor_inc/"util"/"crc.h"] 
+file_paths  = [kern_cor_inc/"util"/"scanner.h"] 
+file_paths  = [kern_cor_inc/"util"/"parser.h"] 
+file_paths  = [kern_cor_inc/"string"/"string.h"] 
+#
+file_paths += [lev2_inc_dir/"ork"/"lev2"/"lev2_types.h"] 
+file_paths += [lev2_gfx_inc/"fxi.h"] 
+file_paths += [lev2_gfx_inc/"gbi.h"] 
+file_paths += [lev2_gfx_inc/"fbi.h"] 
+file_paths += [lev2_gfx_inc/"ci.h"] 
+#
+file_paths += [lev2_gfx_inc/"gfxenv.h"] 
+file_paths += [lev2_gfx_inc/"gfxenv_enum.h"] 
+file_paths += [lev2_gfx_inc/"shadman.h"] 
+file_paths += [lev2_gfx_inc/"texman.h"] 
+file_paths += [lev2_gfx_inc/"rtgroup.h"] 
+file_paths += [lev2_gfx_inc/"gfxmaterial.h"] 
+file_paths += [lev2_gfx_inc/"shadlang.h"] 
+file_paths += [lev2_gfx_inc/"shadlang_nodes.h"] 
+#
+file_paths += [lev2_gfx_inc/"renderer"/"renderer.h"] 
+file_paths += [lev2_gfx_inc/"renderer"/"renderable.h"] 
+file_paths += [lev2_gfx_inc/"renderer"/"rendercontext.h"] 
+file_paths += [lev2_gfx_inc/"renderer"/"renderqueue.h"] 
+file_paths += [lev2_gfx_inc/"renderer"/"renderer_enum.h"] 
+file_paths += [lev2_gfx_inc/"renderer"/"drawable.h"] 
+file_paths += [lev2_gfx_inc/"renderer"/"compositor.h"] 
+file_paths += [lev2_gfx_inc/"renderer"/"NodeCompositor"/"NodeCompositor.h"] 
+file_paths += [lev2_gfx_inc/"renderer"/"NodeCompositor"/"pbr_node_deferred.h"] 
+file_paths += [lev2_gfx_inc/"renderer"/"NodeCompositor"/"pbr_node_forward.h"] 
+
+file_ids = upload_files(file_paths)
 
 #########################################
-
-client = OpenAI(api_key=API_KEY)
-print(client)
-
+# Create an assistant with knowledge retrieval tool
 #########################################
 
-def read_file_lines(file_path):
-    with open(str(file_path), 'r') as file:
-        for line in file:
-            yield line.strip()
+assistant = client.beta.assistants.create(
+  instructions="You are a domain expert of a c++ game engine named Orkid. When asked questions about this engine, respond with the approprate amount of detail to the question.",
+  model=MODEL,
+  tools=[{"type": "retrieval"}],
+  #tools=[{"type": "code_interpreter"}],
+  file_ids=file_ids
+)
+
+thread = client.beta.threads.create()
 
 #########################################
+# Function to ask questions to the assistant
+#########################################
 
-def ask(question,max_tokens=0,interactive=False):
-    if not interactive:
-      print("<<< " + question )
-    if max_tokens!=0:
-      completion = client.chat.completions.create(
-          model=MODEL,
-          messages=[
-            {
-              "role": "user",
-              "content": question,
-            },
-          ],
-          max_tokens=max_tokens   # We're not expecting a meaningful response here
-      )
+def ask_question(question):
+  message = client.beta.threads.messages.create(
+    thread_id = thread.id,
+    file_ids=file_ids,
+    role="user",
+    content=question
+  )
+  run = client.beta.threads.runs.create(
+    thread_id=thread.id,
+    assistant_id=assistant.id
+  )
+  print("waiting for reponse.")
+  keep_waiting = True
+  counter = 0
+  while keep_waiting:
+    run_status = client.beta.threads.runs.retrieve(
+      thread_id = thread.id,
+      run_id = run.id
+    )
+    if run_status.status == "completed":
+      keep_waiting = False
     else:
-      completion = client.chat.completions.create(
-          model=MODEL,
-          messages=[
-            {
-              "role": "user",
-              "content": question,
-            },
-          ],
-      )
-    answer = completion.choices[0].message.content
-    print(">>>>>> " + answer)
-    return answer
-
-#########################################
-
-def prime_context_with_source_file(file_base,file_path):
-    # 
-    # Indicate that we are priming the context with a source file
-    #  strip leading path (file_base) as there is no reason to prime the context with that
-    # 
-    summed_path = str(file_base)+str(file_path)
-    #print(file_base,file_path,summed_path)
-
-    ask(f"--- Start of file named {file_path} ---\n",max_tokens=1 )
-
-    ###################################
-    # Send each line of the file
-    ###################################
-
-    line_array = []
-
-    def send_line_array():
-        if line_array == []:
-            return
-        text = "\n".join(line_array)
-        #print("sending: %s" % text)
-        response = client.chat.completions.create(
-            model=MODEL,
-            messages=[
-            {
-                "role": "user",
-                "content": text,
-            }
-            ],
-            max_tokens=1   #Adjust as needed
-        )
-        #Optionally print or store the response
-        answer = response.choices[0].message.content
-        #print(">>>>>>> " + answer)
-        line_array.clear()
-
-    ###################################
-
-    for line in read_file_lines(summed_path):
-        print("<<: %s" % line)
-        line_array.append(line)
-        if len(line_array) >= 10:
-            send_line_array()
-
-    send_line_array()
-
-    ###################################
-    # Indicate the end of the file
-    ###################################
-
-    ask(f"--- End of {file_path} ---\n",max_tokens=1 )
-
-#########################################
-
-ask("I am cumulatively 'priming' this chatgpt client 'context' with a set ofsourcecode files 10 lines at a time (via pasting into prompt).\n" 
-   +"I do not expect a response yet (until all files are primed)... The file is bounded by '--- Start of <filename>' and '--- End of <filename>' markers.\n"
-   +"Please remember as much as you can about the source code, and the filenames you are about to see. Treat them collectively as source to a single c++/python project - as I will ask questions about it in a following interactive REPL.\n"
-   +"In fact, when I ask 'ENUMERATE' after sending all the files, I want you to enumerate (count) all the c++ class names you detected.\n")
-
-prime_context_with_source_file(core_inc_dir, kern_cor_inc/"svariant.h")
-prime_context_with_source_file(lev2_inc_dir, lev2_gfx_inc/"targetinterfaces.h")
-#prime_context_with_source_file(lev2_inc_dir, lev2_gfx_inc/"fxi.h")
-#prime_context_with_source_file(lev2_inc_dir, lev2_gfx_inc/"imi.h")
-#prime_context_with_source_file(lev2_inc_dir, lev2_gfx_inc/"mtxi.h")
-#prime_context_with_source_file(lev2_inc_dir, lev2_gfx_inc/"fbi.h")
-#prime_context_with_source_file(lev2_inc_dir, lev2_gfx_inc/"gbi.h")
-#prime_context_with_source_file(lev2_inc_dir, lev2_gfx_inc/"txi.h")
-#prime_context_with_source_file(lev2_inc_dir, lev2_gfx_inc/"ci.h")
-#prime_context_with_source_file(lev2_inc_dir, lev2_gfx_inc/"dwi.h")
+      counter+=1
+      # print spinning wheel
+      if counter%4 == 0:
+        print("\r|", end = '')
+      elif counter%4 == 1:
+        print("\r/", end = '')
+      elif counter%4 == 2:
+        print("\r-", end = '')
+      elif counter%4 == 3:
+        print("\r\\", end = '')
+      time.sleep(1)
+  messages = client.beta.threads.messages.list(
+    thread_id=thread.id,
+  )
+  answer = []
+  for message in messages:
+    role = message.role
+    content = message.content[0].text.value
+    answer += [content]
+  print("") 
+  return answer[0]
 
 #########################################
 # repl loop
@@ -143,7 +165,6 @@ while(keep_going):
   if q=="done":
     keep_going = False
   else:
-    ask(q,interactive=True)
-
-#ask( 'what did I just prime this context with?')
+    answer = ask_question(q)
+    print("<<<" + deco.key(answer))
 
