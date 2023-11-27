a2df4fb23eb62643bbf21ccd0d6da1befdb16b78
Michael Mayers <michael@tweakoz.com>
2023-11-27 00:48:45 -0700

    ..

--- obt.project/scripts/ork/ix_gdb_extensions.py
+++ obt.project/scripts/ork/ix_gdb_extensions.py
@@ -1,7 +1,52 @@
 import gdb, re
+from parsy import string, seq, regex, Parser, any_char
 from obt import deco
 
 decor = deco.Deco()
+
+###############################################################################
+# Basic parsers
+###############################################################################
+
+# Basic parsers
+identifier = regex(r'[a-zA-Z_][a-zA-Z0-9_]*')
+namespace = seq(identifier, string('::')).combine(lambda ident, colon: ident + colon)
+qualified_identifier = seq(namespace.optional(), identifier).map(lambda x: ''.join(filter(None, x)))
+angle_bracket_open = string('<')
+angle_bracket_close = string('>')
+comma = string(',')
+
+# Recursive parser for template arguments
+@Parser
+def template_arg(input, index):
+  content = yield (template | qualified_identifier).sep_by(comma)
+  return ",".join(filter(None, content))
+
+# Parser for templates
+template = seq(
+    qualified_identifier + angle_bracket_open,
+    template_arg,
+    angle_bracket_close
+).combine(lambda a, b, c: ''.join(filter(None, [a[0], '<', b, '>'])))
+
+# Parser for std::deque, simplifying to keep only the first argument
+std_deque = seq(
+    string('std::deque<'),
+    template_arg.map(lambda args: args.split(',')[0].strip()),
+    angle_bracket_close
+).combine(lambda a, b, c: 'std::deque<' + b + '>')
+
+# Parser for std::queue, handling std::deque with allocator specifically
+std_queue = seq(
+    string('std::queue<'),
+    template_arg.map(lambda args: std_deque.parse(args) if 'std::deque' in args else args.split(',')[0].strip()),
+    angle_bracket_close
+).combine(lambda a, b, c: 'std::queue<' + b + '>')
+
+# General parser that handles any text and std::queue
+general_parser = (
+    std_queue | std_deque | any_char.at_least(1).concat()
+).many()
 
 ###############################################################################
 
@@ -14,14 +59,32 @@ class FilterThreads(gdb.Command):
                                         gdb.COMMAND_USER)
 
   def invoke(self, arg, from_tty):
+    filter_opq_idle = '--no-idle-opq' in arg or '-i' in arg
+    sorted_threads = dict()
     thread = gdb.selected_thread()
     for i in gdb.inferiors():
       for t in i.threads():
         t.switch()
         frame = gdb.newest_frame()
         if frame and "__futex_abstimed_wait_common64" not in frame.name():
+          show = True
+          if filter_opq_idle:
+            if frame and '__GI___clock_nanosleep' in frame.name():
+              t_name = t.name if t.name else "N/A"
+              if t_name == "concurrentQueue":
+                frame_count = 1
+                while frame:
+                  frame = frame.older()
+                  frame_count += 1
+                if frame_count<=10:
+                  show = False
+          if show:
+            sorted_threads[t.num] = t
           # Format and print thread information
-          self.print_thread_info(t)
+    for i in sorted(sorted_threads.keys()):
+      t = sorted_threads[i]
+      t.switch()
+      self.print_thread_info(t)
 
     # Switch back to the original thread
     if thread:
@@ -48,13 +111,17 @@ class FilterThreads(gdb.Command):
 
 ###############################################################################
 
+
 class RGB:
+
   def __init__(self, r, g, b):
     self.r = r
     self.g = g
     self.b = b
 
+
 class Segment:
+
   def __init__(self, level, text):
     self.level = level
     self.text = text
@@ -70,18 +137,18 @@ class FilteredBacktrace(gdb.Command):
                                             gdb.COMMAND_STACK)
 
     self.level_color_map = {
-        0: RGB(192, 192, 192),  
-        1: RGB(96, 224, 128),  
-        2: RGB(96, 255, 224),  
-        3: RGB(32, 255, 192),  
-        4: RGB(0, 255, 0),  
-        5: RGB(255, 255, 255),  
-        100: RGB(255, 255, 128),  
+        0: RGB(192, 192, 192),
+        1: RGB(96, 224, 128),
+        2: RGB(96, 255, 224),
+        3: RGB(32, 255, 192),
+        4: RGB(0, 255, 0),
+        5: RGB(255, 255, 255),
+        100: RGB(255, 255, 128),
     }
 
   ############################################################
 
-  def create_segments(self,s):
+  def create_segments(self, s):
     level = 0
     segments = []
     temp_str = ""
@@ -90,47 +157,33 @@ class FilteredBacktrace(gdb.Command):
     #####################################
 
     for char in s:
-        if char in "<(":
-            if temp_str:
-                segments.append(Segment(level, temp_str))
-                temp_str = ""
-            segments.append(Segment(symbol_level, char))
-            level += 1
-        elif char in ">)":
-            if temp_str:
-                segments.append(Segment(level, temp_str))
-                temp_str = ""
-            segments.append(Segment(symbol_level, char))
-            level = max(level - 1, 0)  # Ensure level doesn't go below 0
-        else:
-            temp_str += char
+      if char in "<(":
+        if temp_str:
+          segments.append(Segment(level, temp_str))
+          temp_str = ""
+        segments.append(Segment(symbol_level, char))
+        level += 1
+      elif char in ">)":
+        if temp_str:
+          segments.append(Segment(level, temp_str))
+          temp_str = ""
+        segments.append(Segment(symbol_level, char))
+        level = max(level - 1, 0)  # Ensure level doesn't go below 0
+      else:
+        temp_str += char
 
     #####################################
 
     if temp_str:
-        segments.append(Segment(level, temp_str))
+      segments.append(Segment(level, temp_str))
 
     #####################################
 
     return segments
 
   ############################################################
-  
-  def simplify_stl_containers(self,s):
-    # Pattern to match STL containers with default allocator
-    pattern = r'(std::\w+)<([^,>]+), std::\w+<[^>]+>>+'
 
-    # Function to replace matched pattern
-    def replacer(match):
-        container_type = match.group(1)
-        contained_type = match.group(2)
-        return f"{container_type}<{contained_type}>"
-
-    return re.sub(pattern, replacer, s)
-  
-  ############################################################
-
-  def replace_std_string(self,s):
+  def simplify_string(self, s):
     std_string_long = "std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >"
     std_string_short = "std::string"
     return s.replace(std_string_long, std_string_short)
@@ -139,13 +192,16 @@ class FilteredBacktrace(gdb.Command):
 
   def colorizeFrameName(self, s):
     result = ""
-    s = self.replace_std_string(s)
-    s = self.simplify_stl_containers(s)
+    if s == None:
+      return ""
+    s = self.simplify_string(s)
+    parsed = general_parser.parse(s)
+    s = ''.join(filter(None, parsed))
     segments = self.create_segments(s)
     for segment in segments:
-        color = self.level_color_map.get(segment.level, RGB(255, 255, 255))
-        colored_text = decor.rgbstr(color.r, color.g, color.b, segment.text)
-        result += colored_text
+      color = self.level_color_map.get(segment.level, RGB(255, 255, 255))
+      colored_text = decor.rgbstr(color.r, color.g, color.b, segment.text)
+      result += colored_text
     return result
 
   ############################################################
@@ -164,8 +220,6 @@ class FilteredBacktrace(gdb.Command):
               (lev_str, lin_str, fil_str))
 
         print("      %s " % name_str)
-        #print(
-        #    f"# {frame.level()} in {frame.name()} at {frame_file}:{sal.line}")
       frame = frame.older()
 
   @staticmethod
