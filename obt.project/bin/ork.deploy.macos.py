4963a205708230d3aa7139992f62c8de769cb928
Michael T. Mayers <michael@tweakoz.com>
2023-12-17 11:39:21 -0700

    ..

--- obt.project/bin/ork.deploy.macos.py
+++ obt.project/bin/ork.deploy.macos.py
@@ -7,7 +7,7 @@ deco = deco.Deco()
 
 # Helper Functions
 def copy_tree(src, dest):
-    shutil.copytree(str(src), str(dest))
+    shutil.copytree(str(src), str(dest),symlinks=True)
 
 def mkdir(folder, wipe=False):
     if wipe and folder.exists():
@@ -19,10 +19,13 @@ parser = argparse.ArgumentParser(description="python packager")
 parser.add_argument("executable_name", help="Name of the executable (without path).")
 parser.add_argument("exec_args", nargs=argparse.REMAINDER, help="Arguments for the executable.")
 parser.add_argument("--bundlename", type=str, default = "Orkid", help="Bundle name for the app (CFBundleName).", required=True)
+parser.add_argument("--rebase", action="store_true", help="Bundle name for the app (CFBundleName).")
 args = parser.parse_args()
 
 # Import _debug_helpers after parsing arguments
 import _debug_helpers
+
+do_rebase = args.rebase
 
 # Get executable path and arguments
 exe_path, exe_args, exe_name = _debug_helpers.get_exec_and_args(args)
@@ -55,7 +58,15 @@ src_homebrew = Path("/opt/homebrew")
 dst_homebrew = bundle_dir/"MacOS"/"homebrew"
 src_pyvenv = path.stage() / "pyvenv"
 dst_pyvenv = bundle_dir / "MacOS" / "pyvenv"
-dst_python_executable = dst_bin/"python3.11"
+
+dst_python_executable = dst_pyvenv/"bin"/"python3.11"
+
+if is_python_script:
+  exe_base = "../pyvenv/bin/python3.11"
+
+if do_rebase:
+  dst_python_executable = dst_bin/"python3.11"
+
 dst_executable = dst_bin / exe_base
 
 ####################################################################################    
@@ -98,10 +109,18 @@ env_vars = {{
     "PATH": str(this_dir / "bin"),
     "LD_LIBRARY_PATH": str(this_dir / "lib"),
     "DYLD_LIBRARY_PATH": str(this_dir / "lib"),
+    "PATH": "{orig_path}",
+"""
+
+if do_rebase:
+  # OVERRIDE PYTHONPATH, PYTHONHOME
+  init_env_script += f"""
     "PYTHONHOME": str(this_dir / "pyvenv"),
     "PYTHONPATH": str(this_dir / "pyvenv" / "lib" / "python3.11" / "site-packages"),
-    "PATH": "{orig_path}",
-}}
+"""
+
+init_env_script += f"""
+    }}
 
 os.environ.update(env_vars)
 for item in env_vars.keys():
@@ -136,11 +155,19 @@ if not int_executable_path.exists():
   raise FileNotFoundError("Executable " + int_executable_name + " not found @ " + str(int_executable_path) + "(in bundle).")
 
 # Execute the main application with rebased arguments
-cmdlist = [str(int_executable_path)] #+ exe_args
+"""
+
+if do_rebase:
+  init_env_script += f"""cmdlist = [str(int_executable_path)] #+ exe_args
 print("EXEC: " + " ".join(cmdlist))
 subprocess.run(cmdlist)
 """
-  
+else:
+  init_env_script += f"""cmdlist = [str(int_executable_path)] + exe_args
+print("EXEC: " + " ".join(cmdlist))
+subprocess.run(cmdlist)
+"""
+
 ####################################################################################    
 # Create Info.plist
 ####################################################################################    
@@ -205,8 +232,143 @@ copy_tree(orkid_src_dir / "ork.lev2" / "examples", bundle_dir / "Resources" / "o
 copy_tree(orkid_src_dir / "ork.lev2" / "pyext" / "tests", bundle_dir / "Resources" /  "ork.lev2" / "pyext" / "tests")
 copy_tree(orkid_src_dir / "ork.ecs" / "examples", bundle_dir / "Resources" / "ork.ecs" / "examples")
 
-command.run(["mv", dst_pyvenv/"bin"/"python3.11", dst_bin/"python3.11"])
-command.run(["mv", dst_pyvenv/"lib"/"libpython3.11.dylib", dst_lib/"libpython3.11.dylib"])
+if do_rebase:
+  command.run(["mv", dst_pyvenv/"bin"/"python3.11", dst_bin/"python3.11"])
+  command.run(["mv", dst_pyvenv/"lib"/"libpython3.11.dylib", dst_lib/"libpython3.11.dylib"])
+
+  ####################################################################################    
+
+  class MachoReplaceItem(object):
+    def __init__(self, str_search, str_replace):
+      self.str_search = str(str_search)
+      self.str_replace = str(str_replace)
+    def replaceOnMachoBinary(self, mach_o_path):
+      macos.macho_replace_loadpaths(mach_o_path,self.str_search,self.str_replace)
+
+  ####################################################################################    
+
+  print( "Collecting homebrew references" )
+
+  homebrew_refDB = macos.DylibReferenceDatabase()
+  homebrew_refDB.probe_in(src_homebrew,pathtools.patglob(bundle_dir/"MacOS"/"lib","*.dylib"))
+  homebrew_refDB.probe_in(src_homebrew,pathtools.patglob(bundle_dir/"MacOS"/"pyvenv"/"lib","*.dylib"))
+
+  print("HOMEBREW REFERENCERS")
+  homebrew_refDB.dump_referencers()
+  print("HOMEBREW REFERENCES")
+  homebrew_refDB.dump_references()
+
+  print( "Collecting homebrew self-references" )
+
+  homebrew_refDB.references.add(src_homebrew/"opt"/"zstd"/"lib"/"libzstd.1.dylib")
+  homebrew_refDB.references.add(src_homebrew/"opt"/"xz"/"lib"/"liblzma.5.dylib")
+  homebrew_refDB.references.add(src_homebrew/"opt"/"jpeg-turbo"/"lib"/"libjpeg.8.dylib")
+  homebrew_self_references = set()
+  homebrew_references_remapped = set()
+  for item in homebrew_refDB.references:
+    deps = macos.macho_enumerate_dylibs(item)
+    dest = bundle_dir/"MacOS"/"lib"/os.path.basename(item)
+    print("item:" , item)
+    print("  dest: ", dest)
+    shutil.copy(item, dest)
+    homebrew_references_remapped.add(dest)
+    for dep in deps:
+      if str(src_homebrew) in dep:
+        homebrew_self_references.add(dep)
+
+  #################
+        
+  pymod_base = dst_pyvenv/"lib"/"python3.11"/"site-packages"/"orkengine"
+  pymods = macos.enumerateOrkPyMods(pymod_base)
+  print(pymod_base)
+  print(pymods)
+
+  macho_replacements = []
+  macho_replacements.append(MachoReplaceItem("@rpath","@executable_path/../lib"))
+  macho_replacements.append(MachoReplaceItem(path.libs(),"@executable_path/../lib"))
+  macho_replacements.append(MachoReplaceItem("/opt/homebrew/opt/mpfr/lib","@executable_path/../lib"))
+  macho_replacements.append(MachoReplaceItem("/opt/homebrew/opt/gmp/lib","@executable_path/../lib"))
+
+  for item in pymods:
+    for repl in macho_replacements:
+      repl.replaceOnMachoBinary(item)
+    macos.macho_dump(item)
+
+  ################################
+  # fixup dylib paths in dylibs
+  ################################
+
+  def is_candidate(line):
+    rval  = line.startswith('lib')
+    rval = rval or "@rpath" in line 
+    rval = rval or str(src_homebrew) in line 
+    rval = rval or str(path.stage()) in line 
+    rval = rval and '.dylib' in line
+    #print(line,rval)
+    return rval
+
+  # List all dylib files in the directory
+  dylibs = [f for f in os.listdir(dst_lib) if f.endswith('.dylib')]
+  for dylib in dylibs:
+    dylib_path = os.path.join(dst_lib, dylib)
+    # change id of dylib
+    new_id = f'@executable_path/../lib/{dylib}'
+    subprocess.run(['install_name_tool', '-id', new_id, dylib_path])
+    # Get the dependencies of the dylib
+    output = subprocess.check_output(['otool', '-L', dylib_path]).decode()
+    # Iterate over each line of the otool output
+    for line in output.splitlines():
+      line = line.strip()
+      # Check if the line represents a non-rooted dylib reference
+      if is_candidate(line):
+        # Extract the original dylib name
+        original_dylib_name = line.split(' ')[0]
+        # Construct the new rooted path without stripping off version code
+        X = original_dylib_name.split('/')
+        new_dylib_path = f'@executable_path/../lib/{X[-1]}'
+        # Use install_name_tool to change the reference
+        subprocess.run(['install_name_tool', '-change', original_dylib_name, new_dylib_path, dylib_path])
+    macos.macho_dump(dylib_path)
+
+  ################################
+  # fixup dylib paths in executables
+  ################################
+
+  # List all dylib files in the directory
+  binaries = [f for f in os.listdir(dst_bin)]
+  for binary in binaries:
+    binary_path = os.path.join(dst_bin, binary)
+    # Get the dependencies of the dylib
+    output = subprocess.check_output(['otool', '-L', binary_path]).decode()
+    # Iterate over each line of the otool output
+    for line in output.splitlines():
+      line = line.strip()
+      # Check if the line represents a non-rooted dylib reference
+      if is_candidate(line):
+        # Extract the original dylib name
+        original_dylib_name = line.split(' ')[0]
+        # Construct the new rooted path without stripping off version code
+        X = original_dylib_name.split('/')
+        new_dylib_path = f'@executable_path/../lib/{X[-1]}'
+        # Use install_name_tool to change the reference
+        command.run(['install_name_tool', '-change', original_dylib_name, new_dylib_path, binary_path],do_log=True)
+    macos.macho_dump(binary_path)
+
+  ################################
+  # fixup python extension modules
+  ################################
+
+  command.run(["install_name_tool","-change",
+                src_pyvenv/"lib"/"libpython3.11.dylib",
+                "@executable_path/../lib/libpython3.11.dylib",
+                dst_pyvenv/"lib"/"python3.11"/"site-packages"/"orkengine"/"core"/"_core.so"],do_log=True)
+
+  command.run(["install_name_tool","-change",
+                src_pyvenv/"lib"/"libpython3.11.dylib",
+                "@executable_path/../lib/libpython3.11.dylib",
+                dst_pyvenv/"lib"/"python3.11"/"site-packages"/"orkengine"/"lev2"/"_lev2.so"],do_log=True)
+
+  print("Dependencies packaged and RPATHs modified.")
 
 ####################################################################################    
 # Remove __pycache__ directories
@@ -220,216 +382,29 @@ os.system( "find . -type d -name 'tests' -exec rm -rf {} +")
 os.system( "find . -type d -name 'test' -exec rm -rf {} +")
 os.system( "find . -type d -name '*.dist-info' -exec rm -rf {} +")
 
-####################################################################################    
-# fixup RPATHS on copied binaries
-####################################################################################    
-
-def homebrew_dylib_dependencies(raw_deps):
-  rval = []
-  for item in raw_deps:
-    if str(src_homebrew) in item:
-      rval.append(item)
-  return rval
-
-def staging_dylib_dependencies(raw_deps):
-  rval = []
-  for item in raw_deps:
-    if str(path.stage()) in item:
-      rval.append(item)
-  return rval
-
-####################################################################################    
-
-
-def add_rpaths(binary_path, rpaths):
-  """Add the RPATHs to the binary."""
-  for item in rpaths:
-    cmdlist = ['install_name_tool', '-add_rpath', item, binary_path]
-    command.run(cmdlist,do_log=True)
-
-####################################################################################    
-
-class MachoReplaceItem(object):
-  def __init__(self, str_search, str_replace):
-    self.str_search = str(str_search)
-    self.str_replace = str(str_replace)
-  def replaceOnMachoBinary(self, mach_o_path):
-    macos.macho_replace_loadpaths(mach_o_path,self.str_search,self.str_replace)
-
-####################################################################################    
-
-print( "Collecting homebrew references" )
-
-homebrew_refDB = macos.DylibReferenceDatabase()
-homebrew_refDB.probe_in(src_homebrew,pathtools.patglob(bundle_dir/"MacOS"/"lib","*.dylib"))
-homebrew_refDB.probe_in(src_homebrew,pathtools.patglob(bundle_dir/"MacOS"/"pyvenv"/"lib","*.dylib"))
-
-print("HOMEBREW REFERENCERS")
-homebrew_refDB.dump_referencers()
-print("HOMEBREW REFERENCES")
-homebrew_refDB.dump_references()
-
-print( "Collecting homebrew self-references" )
-
-homebrew_self_references = set()
-homebrew_references_remapped = set()
-for item in homebrew_refDB.references:
-  deps = macos.macho_enumerate_dylibs(item)
-  dest = bundle_dir/"MacOS"/"lib"/os.path.basename(item)
-  print("item:" , item)
-  print("  dest: ", dest)
-  shutil.copy(item, dest)
-  homebrew_references_remapped.add(dest)
-  for dep in deps:
-    if str(src_homebrew) in dep:
-      homebrew_self_references.add(dep)
-#print("HOMEBREW SELF REFERENCES")
-#for item in homebrew_self_references:
-#  print(item)
-
-for item in homebrew_references_remapped:
-  dylib_paths = macos.macho_enumerate_dylibs(item)
-  for inpitem in dylib_paths:
-    if str(src_homebrew) in str(inpitem):
-      p = "@executable_path/../"+os.path.basename(inpitem)
-      print(p, inpitem)
-      command.run(["install_name_tool","-change",inpitem,p,item],do_log=True)
-      macos.macho_dump(item)
-
-# add homebrew self references to homebrew_references
-#assert(False)
-# rebase all deps to bundle relative
-
-all_scan = macos.macho_get_all_dylib_dependencies(exe_path)
-
-def remap_to_bundle(paths):
-  staging_dest_folder = bundle_dir/"MacOS"
-  rval = []
-  pyvlib = path.stage()/"pyvenv"/"lib"
-  for item in paths:
-    if str(src_homebrew) in str(item):
-      item = item.replace(str(src_homebrew), str(dst_homebrew))
-    elif str(pyvlib) in item:
-      item = item.replace(str(pyvlib), str(staging_dest_folder/"lib"))
-    elif str(path.stage()) in item:
-      item = item.replace(str(path.stage()), str(staging_dest_folder))
-    rval.append(item)
-  return rval
-
-
-bundle_remapped = remap_to_bundle(all_scan)
-
-hb_deps = homebrew_dylib_dependencies(bundle_remapped)
-staging_deps = staging_dylib_dependencies(bundle_remapped)
-print("all_scan: ", all_scan)
-print("bundle_remapped: ", bundle_remapped)
-print("hb_deps: ", hb_deps)
-print("staging_deps: ", staging_deps)
-#assert(False)
-
-pymod_base = dst_pyvenv/"lib"/"python3.11"/"site-packages"/"orkengine"
-pymods = macos.enumerateOrkPyMods(pymod_base)
-print(pymod_base)
-print(pymods)
-
-macho_replacements = []
-macho_replacements.append(MachoReplaceItem("@rpath","@executable_path/../lib"))
-macho_replacements.append(MachoReplaceItem(path.libs(),"@executable_path/../lib"))
-macho_replacements.append(MachoReplaceItem("/opt/homebrew/opt/mpfr/lib","@executable_path/../lib"))
-macho_replacements.append(MachoReplaceItem("/opt/homebrew/opt/gmp/lib","@executable_path/../lib"))
-
-for item in pymods:
-  for repl in macho_replacements:
-    repl.replaceOnMachoBinary(item)
-  macos.macho_dump(item)
-
-############
-
-def change_rpaths_staging(binary_path):
-  if str(src_homebrew) in str(binary_path):
-    return
-  output = subprocess.check_output(['otool', '-L', binary_path]).decode()
-  print(deco.rgbstr(255,255,192,output))
-  deco_binpath = deco.rgbstr(128,255,255,binary_path)
-  print(f"RPATH: {deco_binpath}")
-  splitlines = output.splitlines()
-  num_lines = len(splitlines)
-  check_children = []
-  for line_index in range(1,num_lines):
-    line = splitlines[line_index]
-    is_install_name = (line_index==1)
-    is_reference = (line_index>1)
-    if is_install_name and (str(path.stage()) in line):
-      lib_path = line.split()[0]
-      lib_name = "@executable_path/../lib/"+os.path.basename(lib_path)
-      deco_libpath = deco.key(lib_path)
-      deco_libname = deco.orange(lib_name)
-      print("CH.INST.NAME")
-      print(f"     : {deco_libpath}")
-      print(f"    -> {deco_libname}")
-      cmdlist = ['install_name_tool', '-change', lib_path, lib_name, binary_path]
-      command.run(cmdlist,do_log=True)
-    elif is_reference and (str(path.stage()) in line):
-      lib_path = line.split()[0]
-      lib_name = "@executable_path/../lib/"+os.path.basename(lib_path)
-      deco_libpath = deco.key(lib_path)
-      deco_libname = deco.orange(lib_name)
-      print("CH.REF.NAME<ST>")
-      print(f"     : {deco_libpath}")
-      print(f"    -> {deco_libname}")
-      cmdlist = ['install_name_tool', '-change', lib_path, lib_name, binary_path]
-      command.run(cmdlist,do_log=True)
-      check_children.append(lib_path)
-    elif is_reference and (str(src_homebrew) in line):
-      lib_path = line.split()[0]
-      lib_name = "@executable_path/../lib/"+os.path.basename(lib_path)
-      deco_libpath = deco.key(lib_path)
-      deco_libname = deco.orange(lib_name)
-      print("CH.REF.NAME<HB>")
-      print(f"     : {deco_libpath}")
-      print(f"    -> {deco_libname}")
-      cmdlist = ['install_name_tool', '-change', lib_path, lib_name, binary_path]
-      command.run(cmdlist,do_log=True)
-      check_children.append(lib_path)
-  for item in check_children:
-    change_rpaths_staging(item)
-
-change_rpaths_staging(dst_executable)
-for dep in hb_deps:
-  change_rpath(dep, dst_homebrew)
-for dep in staging_deps:
-  change_rpaths_staging(dep)
-#for dep in homebrew_referencers:
-#  change_rpaths_staging(dep)
-
-
-os.system("install_name_tool -change libpython3.11.dylib @executable_path/../lib/libpython3.11.dylib " + str(dst_python_executable))
-os.system("install_name_tool -add_rpath . " + str(dst_python_executable))
-
-print("Dependencies packaged and RPATHs modified.")
-
 ################################
 # write site customizer
 ################################
 
-print("Customizing Python to Bundle....")
+if do_rebase:
+  print("Customizing Python to Bundle....")
 
-A = """import sys
-import os
+  A = """import sys
+  import os
 
-# Get PYTHONHOME environment variable
-python_home = os.environ.get('PYTHONHOME', '')
+  # Get PYTHONHOME environment variable
+  python_home = os.environ.get('PYTHONHOME', '')
 
-# Keep only paths that start with PYTHONHOME in sys.path
-sys.path = [p for p in sys.path if p.startswith(python_home)]
-"""
-with open(dst_pyvenv/"lib"/"python3.11"/"site-packages"/"sitecustomize.py","w") as file:
-  file.write(A)
+  # Keep only paths that start with PYTHONHOME in sys.path
+  sys.path = [p for p in sys.path if p.startswith(python_home)]
+  """
+  with open(dst_pyvenv/"lib"/"python3.11"/"site-packages"/"sitecustomize.py","w") as file:
+    file.write(A)
 
-os.environ["PYTHONHOME"] = str(dst_pyvenv)
-os.environ["PYTHONPATH"] = str(dst_pyvenv/"lib"/"python3.11"/"site-packages")
-# Path to the bundled Python executable
-python_executable = str(bundle_dir / "MacOS" / "bin" / "python3.11")
+  os.environ["PYTHONHOME"] = str(dst_pyvenv)
+  os.environ["PYTHONPATH"] = str(dst_pyvenv/"lib"/"python3.11"/"site-packages")
+  # Path to the bundled Python executable
+  python_executable = str(bundle_dir / "MacOS" / "bin" / "python3.11")
 
-# Install ork.build package into the bundled Python environment
-subprocess.run([python_executable, "-m", "pip", "install", "ork.build"])
+  # Install ork.build package into the bundled Python environment
+  subprocess.run([python_executable, "-m", "pip", "install", "ork.build"])
