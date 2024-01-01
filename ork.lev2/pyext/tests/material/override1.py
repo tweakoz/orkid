e808e2c24df6038576eaedf1defe3a266150d25a
Michael T. Mayers <michael@tweakoz.com>
2023-12-31 22:24:47 -0700

    ..

--- ork.lev2/pyext/tests/material/override1.py
+++ ork.lev2/pyext/tests/material/override1.py
@@ -73,6 +73,7 @@ class SceneGraphApp(object):
     handled = self.uicam.uiEventHandler(uievent)
     if handled:
       self.camera.copyFrom( self.uicam.cameradata )
+    return ui.HandlerResult()
 
   ################################################
 
