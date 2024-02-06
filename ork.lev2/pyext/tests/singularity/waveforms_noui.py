0a00262922470fde39c6db678ebe9f23c739a3a3
Michael T. Mayers <michael@tweakoz.com>
2024-02-05 20:44:42 -0700

    ..

--- ork.lev2/pyext/tests/singularity/waveforms_noui.py
+++ ork.lev2/pyext/tests/singularity/waveforms_noui.py
@@ -1,44 +1,29 @@
 #!/usr/bin/env python3
 
 ################################################################################
-# singularity test for editing layers in a soundbank
-# Copyright 1996-2023, Michael T. Mayers.
+# singularity minimal sound playback with no UI
+# Copyright 1996-2024, Michael T. Mayers.
 # Distributed under the MIT License
 # see license-mit.txt in the root of the repo, and/or https://opensource.org/license/mit/
 ################################################################################
-
-import numpy as np
-import sys, random, time
+import sys, time
 from orkengine.core import *
 from orkengine.lev2 import *
-from orkengine.lev2 import singularity as S
-
-################################################################################
-sys.path.append((thisdir()/"..").normalized.as_string) # add parent dir to path
 sys.path.append((lev2exdir()/"python").normalized.as_string) # add orkid lev2 python extensions to path
-from _boilerplate import *
-from singularity._harness import SingulTestApp, find_index
 from audio import nodata
-tokens = CrcStringProxy()
 ################################################################################
-
 lev2appinit()
 audiodevice = singularity.device.instance()
 synth = singularity.synth.instance()
-synth.system_tempo = 120.0
-sequencer = synth.sequencer
-
 mainbus = synth.outputBus("main")
-synth.setEffect(mainbus,"Reverb:GuyWire")
-#synth.setEffect(mainbus,"none")
+synth.setEffect(mainbus,"Reverb:GuyWire") # none
 synth.masterGain = singularity.decibelsToLinear(18.0)
 audiodata = nodata.NoDataSynthProgram(do_lfo=True,do_rez=True)
 synth.programbus.uiprogram = audiodata.patch
 voice = synth.keyOn(36,127,audiodata.patch,None)
-##############################################
-
 ###############################################################################
-
+# main loop
+###############################################################################
 while True:
   synth.mainThreadHandler()
   time.sleep(0)
