2df362ac5522e9e41ac3228f89a45e8eee3d82c7
Michael T. Mayers <michael@tweakoz.com>
2024-02-15 00:55:54 -0700

    clean up vowel python test

--- ork.lev2/pyext/tests/singularity/frqdom_vowels.py
+++ ork.lev2/pyext/tests/singularity/frqdom_vowels.py
@@ -18,57 +18,67 @@ sys.path.append((thisdir()/"..").normalized.as_string) # add parent dir to path
 from singularity import _frqdom as frqdom
 ################################################################################
 
+def generateVowelDataset():
+  irdataset = S.SpectralImpulseDataSet()
+  strength = 64.0
+  sirA = S.SpectralImpulseResponse()
+  sirA.vowelFormant('A',strength)
+  sirE = S.SpectralImpulseResponse()
+  sirE.vowelFormant('E',strength)
+  sirI = S.SpectralImpulseResponse()
+  sirI.vowelFormant('I',strength)
+  sirO = S.SpectralImpulseResponse()
+  sirO.vowelFormant('O',strength)
+  sirU = S.SpectralImpulseResponse()
+  sirU.vowelFormant('U',strength)
+  
+  COUNT = 8
+  S0 = COUNT//4
+  S1 = COUNT//2
+  S2 = COUNT*3//4
+  irdataset.resize(COUNT)
+  
+  for i in range(0,COUNT):
+    sir = S.SpectralImpulseResponse()
+    if( i<S0 ):
+      fi = i/float(S0)
+      sir.blend(sirA,sirE,fi)
+    elif( i<S1 ):
+      fi = (i-S0)/float(S0)
+      sir.blend(sirE,sirI,fi)
+    elif( i<S2 ):
+      fi = (i-S1)/float(S0)
+      sir.blend(sirI,sirO,fi)
+    else:
+      fi = (i-S2)/float(S0)
+      sir.blend(sirO,sirU,fi)   
+    irdataset.set(i, sir)
+  return irdataset
+
+################################################################################
+
 class TestApp(frqdom.WaveformsApp):
   def __init__(self):
     super().__init__()
   ##########################################
   def modLayer(self,newlyr):
-    irdataset = S.SpectralImpulseDataSet()
-    strength = 64.0
-    sirA = S.SpectralImpulseResponse()
-    sirA.vowelFormant('A',strength)
-    sirE = S.SpectralImpulseResponse()
-    sirE.vowelFormant('E',strength)
-    sirI = S.SpectralImpulseResponse()
-    sirI.vowelFormant('I',strength)
-    sirO = S.SpectralImpulseResponse()
-    sirO.vowelFormant('O',strength)
-    sirU = S.SpectralImpulseResponse()
-    sirU.vowelFormant('U',strength)
-    
-    COUNT = 8
-    S0 = COUNT//4
-    S1 = COUNT//2
-    S2 = COUNT*3//4
-    irdataset.resize(COUNT)
-    
-    for i in range(0,COUNT):
-      sir = S.SpectralImpulseResponse()
-      if( i<S0 ):
-        fi = i/float(S0)
-        sir.blend(sirA,sirE,fi)
-      elif( i<S1 ):
-        fi = (i-S0)/float(S0)
-        sir.blend(sirE,sirI,fi)
-      elif( i<S2 ):
-        fi = (i-S1)/float(S0)
-        sir.blend(sirI,sirO,fi)
-      else:
-        fi = (i-S2)/float(S0)
-        sir.blend(sirO,sirU,fi)   
-      irdataset.set(i, sir)
-############################
+    # fetch dsp stage
     dspstg = newlyr.stage("DSP")
+    # create post-effect dsp blocks
     frqdom = dspstg.appendDspBlock("ToFrequencyDomain","2frq")
     vowels = dspstg.appendDspBlock("SpectralConvolve","sop4")
-    vowels.dataset = irdataset
     timdom = dspstg.appendDspBlock("ToTimeDomain","2tim")
-    lfo = newlyr.appendController("Lfo","lfo")
+    # create LFO
+    lfo = newlyr.appendController("Lfo","vowel-lfo")
     lfo.minRate = 1
     lfo.maxRate = 1
-    vowels.paramByIndex(0).mods.src1 = lfo
-    vowels.paramByIndex(0).mods.src1scale = 0.5
-    vowels.paramByIndex(0).mods.src1bias = 0.5
+    # set LFO as modulator
+    spec_index_param = vowels.paramByIndex(0)
+    spec_index_param.mods.src1 = lfo
+    spec_index_param.mods.src1scale = 0.5
+    spec_index_param.mods.src1bias = 0.5
+    # assign IR dataset
+    vowels.dataset = generateVowelDataset()
     
 
 ###############################################################################
