5b08f405bb2c2be7c8090846fa208f34da4b1d80
Michael T. Mayers <michael@tweakoz.com>
2024-11-13 23:56:44 -0700

    stereo points test

--- obt.project/scripts/ork/path.py
+++ obt.project/scripts/ork/path.py
@@ -1,6 +1,21 @@
+from obt import path as ork_path
 import os 
-from obt import path as obt_path
 
-root = obt_path.orkid()
-lev2 = root/"ork.lev2"
-lev2_pylib = lev2/"examples"/"python"
+
+def _root():
+	return ork_path.Path(os.environ["ORKID_WORKSPACE_DIR"])
+
+def __getattr__(name):
+  if name == "root":
+  	return _root()
+  elif name == "lev2":
+  	return _root()/"ork.lev2"
+  elif name == "lev2_pylib":
+  	return _root()/"ork.lev2"/"examples"/"python"
+  elif name == "render_tests":
+  	return _root()/"ork.lev2"/"pyext"/"tests"/"renderer"
+  elif name == "py_examples":
+  	return _root()/"ork.lev2"/"examples"/"python"
+  elif name == "py_lev2utils":
+  	return _root()/"ork.lev2"/"examples"/"python"/"lev2utils"
+  return None
