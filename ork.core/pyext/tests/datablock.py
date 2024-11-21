79c563e7d0d8f364c47c8c5b9701b1463c49ec80
Michael T. Mayers <michael@tweakoz.com>
2024-11-20 23:56:51 -0700

    fix datablock test

--- ork.core/pyext/tests/datablock.py
+++ ork.core/pyext/tests/datablock.py
@@ -3,11 +3,39 @@
 import numpy as np
 from orkengine.core import *
 
+##############################################
+# write datablock
+##############################################
+
 a = np.ones([200, 300, 3],dtype = np.uint8)
 db = DataBlock()
-db.addData(a)
-print(db.size)
-print(a.shape)
+db.writeString("np.uint8") # type
+db.writeInt(3) # ndim
+db.writeInt(300) # width
+db.writeInt(200) # height
+db.writeInt(3) # depth
+a_as_bytes = a.tobytes() # convert to bytes
+db.writeBytes(a_as_bytes) # write to datablock
+
 print(db)
 
-db.wtf(db)
+##############################################
+# read datablock
+###############################################
+
+istream = DataBlockInputStream(db)
+type = istream.readString()
+ndim = istream.readInt()
+width = istream.readInt()
+height = istream.readInt()
+depth = istream.readInt()
+bytes = istream.readBytes()
+
+###############################################
+
+print(type)
+print(ndim)
+print(width)
+print(height)
+print(depth)
+print(len(bytes))
