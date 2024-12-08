434a9954b35ea6e794d022e10575f82056556655
Michael T. Mayers <michael@tweakoz.com>
2024-12-07 19:12:09 -0700

    ..

--- ork.lev2/src/gfx/gl/glcommon.cpp
+++ ork.lev2/src/gfx/gl/glcommon.cpp
@@ -30,7 +30,7 @@ void GlPlatformObject::makeCurrent() {
     _current = this;
     if(_ctxbase){
       auto window = _ctxbase->_glfwWindow;
-      printf( "_glfwWindow<%p> made current\n", (void*) window );
+      //printf( "_glfwWindow<%p> made current\n", (void*) window );
       glfwMakeContextCurrent(window);
     }
     else{
