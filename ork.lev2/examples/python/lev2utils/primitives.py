59cf88beae781c845ff49b4b43b18c4cff4b8124
Michael T. Mayers <michael@tweakoz.com>
2025-05-23 13:51:51 -0600

    boilerplate reduction

--- ork.lev2/examples/python/lev2utils/primitives.py
+++ ork.lev2/examples/python/lev2utils/primitives.py
@@ -218,3 +218,141 @@ def createParticleData( use_streaks = True ):
       self.drawable_data.graphdata = self.graphdata
 
   return ImplObject()
+
+
+def createImposter( context = None,
+                    radius     = 1.0, 
+                    detail     = 1, 
+                    shaderpath = None,
+                    shadertek  = None,
+                    layer = None,
+                    DIM = 128,
+                    is_stereo = False ):
+
+  assert(shaderpath!=None)
+  assert(shadertek!=None)
+  assert(layer!=None)
+  assert(context!=None)
+  class ImposterObject(object):
+
+    def __init__(self):
+      super().__init__()
+      imp_mtl = FreestyleMaterial()
+      imp_mtl.gpuInit(context,str(shaderpath))
+      imp_mtl.rasterstate.setBlendingMacro(tokens.OFF)
+      imp_mtl.rasterstate.culltest = tokens.PASS_FRONT
+      imp_mtl.rasterstate.depthtest = tokens.LEQUALS
+      imp_permu = FxPipelinePermutation()
+      imp_permu.technique = imp_mtl.shader.technique(shadertek)
+      imp_pipeline = imp_mtl.fxcache.findPipeline(imp_permu)
+      imp_pipeline.name = "imppasspipe"
+      imp_pipeline.sharedMaterial = imp_mtl
+      self.impdata = ImposterDrawableData()
+      self.impdata.shape = Sphere(vec3(0), radius)
+      self.impdata.detail = detail
+      self.impdata.imp_pass.pipeline = imp_pipeline
+      self.sgnode = layer.createDrawableNodeFromData("imp1",self.impdata)
+      self.sgnode.worldTransform.scale = 1
+      self.sgnode.worldTransform.translation = vec3(0,0,0)
+      self.imp_mtl = imp_mtl
+      self.is_stereo = is_stereo
+      self.frame_index = 0
+
+      #####################
+      # imposter rtgroup
+      #####################
+
+      imp_pass = self.impdata.imp_pass
+      rtg_imps = [self.createRGBRTG("imprtg0")]
+      if is_stereo:
+        rtg_imps += [self.createRGBRTG("imprtg1")]
+      self.rtg_imp = rtg_imps[0]
+      imp_pass.rtgroup = self.rtg_imp
+      imp_pass.pipeline = imp_pipeline
+      self.rtg_imps = rtg_imps
+    
+    #######################################################
+
+    def onGpuUpdate(self,ctx):
+      self.frame_index += 1
+
+    #######################################################
+    
+    def createRGBRTG(self,name):
+      rtg = RtGroup(context,DIM,DIM)
+      rtg.name = name
+      rtg.createBuffer(tokens.RGBA32F,tokens.NONE)
+      context.FBI.rtGroupInit(rtg)
+      return rtg
+
+    #######################################################
+    # standard feedback (ping-ping) blit pass
+    #  with user specified shader
+    #######################################################
+
+    def installFeedbackBlit(self,shaderpath=None,shadertek=None):
+
+      upass = ImposterPassData()
+      
+      rtg_feedback = [self.createRGBRTG("FB0L"),self.createRGBRTG("FB1L")]
+      if self.is_stereo:
+        rtg_feedback += [self.createRGBRTG("FB0R"),self.createRGBRTG("FB1R")]
+
+      self.fb_tex = rtg_feedback[0].texture(0)
+            
+      upass_mtl = FreestyleMaterial()
+      upass_mtl.gpuInit(context,shaderpath)
+      upass_mtl.rasterstate.setBlendingMacro(tokens.OFF)
+      upass_mtl.rasterstate.culltest = tokens.OFF
+      upass_mtl.rasterstate.depthtest = tokens.OFF
+      upass_mtl.rasterstate.culltest = tokens.OFF
+      upass_mtl.rasterstate.depthtest = tokens.OFF
+      upass_mtl.rasterstate.writeMaskRGB = True
+      upass_mtl.rasterstate.writeMaskA = True
+      upass_mtl.rasterstate.writeMaskZ = True
+      upass_permu = FxPipelinePermutation()
+      upass_permu.technique = upass_mtl.shader.technique(shadertek)
+      upass.pipeline = upass_mtl.fxcache.findPipeline(upass_permu)
+      upass.pipeline.name = "upasspipe"
+      upass.pipeline.sharedMaterial = upass_mtl
+      self.impdata.user_passes = [upass]
+      upass.enabled = True
+
+      def _on_post_render():
+        eye_index = context.topRCFD.userprops.eye_index # 0: left, 1: right
+        eye_index = 0 if (eye_index==None) else eye_index
+        self.rtg_imp = self.rtg_imps[eye_index]
+        self.impdata.imp_pass.rtgroup = self.rtg_imp
+        base = eye_index * 2
+        if (self.frame_index % 2) == 0:
+          upass.rtgroup = rtg_feedback[base+1] # upass renders to fb1
+          self.impdata.blit_pass.userdata.color_rtg = rtg_feedback[base+1] # blit_pass reads fb1
+          self.fb_tex = rtg_feedback[base+0].texture(0) # upass reads fb0
+        else:
+          upass.rtgroup = rtg_feedback[base+0] # upass renders to fb0
+          self.impdata.blit_pass.userdata.color_rtg = rtg_feedback[base+0] # blit_pass reads fb0
+          self.fb_tex = rtg_feedback[base+1].texture(0) # upass reads fb1
+
+      upass.onPostRender(_on_post_render)
+      _on_post_render() # first time init
+      
+      self.feedback_pass = upass
+      self.feedback_mtl = upass_mtl
+
+    #######################################################
+
+    def installStandardBlit(self):
+      def _on_post_render():
+        eye_index = context.topRCFD.userprops.eye_index # 0: left, 1: right
+        eye_index = 0 if (eye_index==None) else eye_index
+        self.rtg_imp = self.rtg_imps[eye_index]
+        self.impdata.imp_pass.rtgroup = self.rtg_imp
+        self.impdata.blit_pass.userdata.color_rtg = self.rtg_imp 
+        self.impdata.blit_pass.userdata.depth_rtg = self.rtg_imp 
+      self.impdata.blit_pass.onPostRender(_on_post_render)
+      _on_post_render() # first time init
+
+    #######################################################
+
+  impobject = ImposterObject()
+  return impobject
