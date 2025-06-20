da5e8e00efaf4d08cd23fdf9f3151ec49d7e4566
Michael T. Mayers <michael@tweakoz.com>
2025-06-20 02:11:22 -0600

    we got triangles

--- ork.lev2/src/gfx/vulkan/vulkan_fxi.cpp
+++ ork.lev2/src/gfx/vulkan/vulkan_fxi.cpp
@@ -19,8 +19,8 @@ VkFxInterface::VkFxInterface(vkcontext_rawptr_t ctx)
     _slp_cache = _GVI->_slp_cache;
 
     _default_rasterstate = std::make_shared<lev2::RasterState>();
-    //_default_rasterstate->_depthtest = EDepthTest::OFF;
-    //_default_rasterstate->_culltest = ECullTest::OFF;
+    _default_rasterstate->_depthtest = EDepthTest::OFF;
+    _default_rasterstate->_culltest = ECullTest::OFF;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
