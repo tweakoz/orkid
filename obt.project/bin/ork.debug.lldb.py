2ceb22cb57fa7ac496572a7cba30d3d6ada4dd4d
Michael T. Mayers <michael@tweakoz.com>
2025-07-21 21:53:43 -0600

    lldb tweak

--- obt.project/bin/ork.debug.lldb.py
+++ obt.project/bin/ork.debug.lldb.py
@@ -11,8 +11,9 @@ parser.add_argument("executable_name", help="Name of the executable (without pat
 parser.add_argument("exec_args", nargs=argparse.REMAINDER, help="Arguments for the executable.")
 args = parser.parse_args()
 
-extensions_py = path.orkid()/"obt.project"/"scripts"/"ork"/"ix_gdb_extensions.py"
-stdcxx_extensions_py = path.Path("/usr/share/gcc/python/libstdcxx/v6/printers.py")
+# gdb 
+# extensions_py = path.orkid()/"obt.project"/"scripts"/"ork"/"ix_gdb_extensions.py"
+# stdcxx_extensions_py = path.Path("/usr/share/gcc/python/libstdcxx/v6/printers.py")
 
 exe_path, exe_args, exe_name = _debug_helpers.get_exec_and_args(args)
 
