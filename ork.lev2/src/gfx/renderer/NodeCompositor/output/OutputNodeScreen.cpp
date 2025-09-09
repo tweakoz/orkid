049cdb432f5e27b08e0b04dbc827db7a1e8f1e0d
Michael T. Mayers <michael@tweakoz.com>
2025-09-09 10:36:03 -0600

    ..

--- ork.lev2/src/gfx/renderer/NodeCompositor/output/OutputNodeScreen.cpp
+++ ork.lev2/src/gfx/renderer/NodeCompositor/output/OutputNodeScreen.cpp
@@ -47,7 +47,7 @@ struct SCRIMPL {
     if (_needsinit) {
       _blit2screenmtl.gpuInit(ctx, "orkshader://blit");
       _blit2screenmtl._rasterstate->setCullTest(ECullTest::OFF);
-      _fxtechnique1x1       = _blit2screenmtl.technique("texcolor");
+      _fxtechnique1x1       = _blit2screenmtl.technique("blit");
       _fxtechnique2x2       = _blit2screenmtl.technique("downsample_2x2");
       _fxtechnique3x3       = _blit2screenmtl.technique("downsample_3x3");
       _fxtechnique4x4       = _blit2screenmtl.technique("downsample_4x4");
