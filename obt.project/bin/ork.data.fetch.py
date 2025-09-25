9bcd2577c1322bfb6eb1359b94d5a5399df013c4
Michael T. Mayers <michael@tweakoz.com>
2025-09-25 14:18:35 -0600

    ork.singularity.data.fetch.py -> ork.data.fetch.py

--- obt.project/bin/ork.data.fetch.py
+++ obt.project/bin/ork.data.fetch.py
@@ -4,3 +4,4 @@ from obt import command
 
 # fetch all singularity namespace assets
 command.run(["ork.asset.catalog.fetch.py", "-n", "singularity"], do_log=True)
+command.run(["ork.asset.catalog.fetch.py", "-n", "ork_envmaps"], do_log=True)
