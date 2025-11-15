ab73e466669c6b2ff6cfa8fb2382003ebfa8b37d
Michael T. Mayers <michael@tweakoz.com>
2025-11-15 15:39:49 -0700

    ..

--- ork.lev2/pyext/tests/renderer/datasources/vdb_lavalamp.py
+++ ork.lev2/pyext/tests/renderer/datasources/vdb_lavalamp.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env ork.python
 
 import sys
-from ork import path as ork_path
-from orkengine.core import vec3, vec4, lev2_pyexdir, VarMap
-from orkengine import lev2
-#sys.path.append(str(ork_path.py_lev2utils)) # add parent dir to path
-#lev2_pyexdir.addToSysPath()
-#from scenegraph import createSceneGraph
 from _lavalamp import LavalampComponent
 from ork.app.application import ComponentizedApplication
 from ork.app.std_scenegraph import StandardSceneGraphComponent
@@ -17,7 +11,6 @@ class LavaLampApp(ComponentizedApplication):
 
   def __init__(self):
     super().__init__()
-
     ############################################
     self.SGC = self.addComponent("std_scenegraph", StandardSceneGraphComponent)
     self.LUI = self.addComponent("loggerui", LoggerUIComponent, filter_regex=[".*"]) 
