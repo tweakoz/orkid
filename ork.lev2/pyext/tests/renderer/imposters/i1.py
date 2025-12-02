805735cec84570657a3c273e6e30e5fea70eb4a1
Michael T. Mayers <michael@tweakoz.com>
2025-12-01 23:58:43 -0700

    fix i1

--- ork.lev2/pyext/tests/renderer/imposters/i1.py
+++ ork.lev2/pyext/tests/renderer/imposters/i1.py
@@ -7,96 +7,52 @@
 # see license-mit.txt in the root of the repo, and/or https://opensource.org/license/mit/
 ################################################################################
 
-import math, random, argparse, sys, signal
-import numpy as np
-from orkengine.core import vec3, vec4, quat, mtx4, Sphere, Transform
-from orkengine.core import CrcStringProxy, lev2_pyexdir, thisdir
+import math, argparse
+import _boilerplate as boilerplate
+from orkengine.core import vec3, mtx4, CrcStringProxy, thisdir
 from orkengine import lev2
-from obt.path import Path       
+from ork.app.std_scenegraph import StandardSceneGraphComponent, StdSpotLight
+from ork.app.std_imposter import createImposter
+from ork.app.std_grid import createGridData
+
+################################################################################
+
 tokens = CrcStringProxy()
         
 ################################################################################
-
-lev2_pyexdir.addToSysPath()
-this_dir = thisdir()
-from lev2utils.cameras import setupUiCamera
-from lev2utils.primitives import createGridData, createImposter
-from lev2utils.scenegraph import createSceneGraph
-from lev2utils.lighting import MySpotLight, MyCookie
-
-################################################################################
-IMP_DIM = 384
+IMP_DIM = 128
 ################################################################################
 
-class ImposterApp(object):
+class ImposterApp(boilerplate.ImposterBaseApp):
 
-  def __init__(self,is_stereo=None,extapp=None):
+  def __init__(self,envmap="cold"):
     super().__init__()
     self.time = 0.0
-    if extapp==None:
-      self.ezapp = lev2.OrkEzApp.create(self,ssaa=0)
-      self.ezapp.setRefreshPolicy(lev2.RefreshFastest, 0)
-    else:
-      self.ezapp = extapp.ezapp
 
-    self.extapp = extapp
-    setupUiCamera(app=self,eye=vec3(0,1,1)*25,tgt=vec3(0,0,0))
+    grid_data = createGridData(extent=1000.0)
+    grid_data.shader_suffix = "_V3"
+    grid_data.modcolor = vec3(1,1.2,1.3)*2
 
-    self.is_stereo = is_stereo
-    self.RENDERMODEL = "FWDPBRVRDM" if is_stereo else "ForwardPBR"
-
-    def onCtrlC(signum, frame):
-      print("signalling EXIT to ezapp")
-      self.ezapp.signalExit()
-
-    signal.signal(signal.SIGINT, onCtrlC)
+    super().__init__(ssaa=1,
+                     grid_data=grid_data,
+                     sg_params = {
+                        "SkyboxTexPathStr": envmap,
+                        "SkyboxIntensity": 0.5,
+                        "SpecularIntensity": 1.0,
+                        "DiffuseIntensity": 1.0,
+                        "AmbientLight": vec3(0.0),
+                        "DepthFogDistance": float(1e5),
+                        "use_float_color_buffer": True
+                     })
 
   ##############################################
 
-  def onGpuInit(self,ctx):
-
-    if self.is_stereo and (self.extapp==None):
-      self.vrdev = lev2.orkidvr.novr_device()
-      self.vrdev.camera = "vrcam"
-      self.vrdev.width = 1280
-      self.vrdev.height = 1280
-      self.vrdev.FOVD = 90
+  def _onGpuInit(self,ctx):
       
-    ###################################
-    # create scenegraph
-    ###################################
-
-    if self.extapp==None:
-      params_dict = {
-        "SkyboxTexPathStr": "cold",
-        "SkyboxIntensity": 0.5,
-        "DiffuseIntensity": 1.0,
-        "SpecularIntensity": 1.0,
-        "AmbientLevel": vec3(0),
-        "DepthFogDistance": 10000.0,
-      }
-      params_dict["preset"] = self.RENDERMODEL
-
-      ##################
-      # create model / sg node
-      ##################
-
-      createSceneGraph(app=self,params_dict=params_dict)
-      self.lyr_donly = self.scene.createLayer("depth_prepass")
-      self.lyr_fwd = self.layer1
-      self.fwd_layers = [self.lyr_fwd,self.lyr_donly]
-     ###################################
-
-    self.grid_data = createGridData(extent=1000)
-
-    self.grid_data.shader_suffix = "_V3"
-    self.grid_data.modcolor = vec3(1,1.2,1.3)*2
-    self.grid_data.majorTileDim = 1.0
-    self.grid_node = self.lyr_fwd.createDrawableNodeFromData("grid",self.grid_data)
-    self.grid_node.sortkey = 1
+    SGC = self.findComponentsByClass(StandardSceneGraphComponent)[0]
+    SG = SGC.scenegraph      
 
     self.ball_model = lev2.XgmModel("data://tests/pbr_calib.glb")
-    self.cookie1 = MyCookie("src://effect_textures/knob2.png")
 
   ##############################################
   # create imposter
@@ -107,11 +63,10 @@ class ImposterApp(object):
                                filtertype=tokens.BILINEAR,
                                filterradius=3.0, 
                                detail=2,
-                               shaderpath=this_dir/"i1.fxv2",
+                               shaderpath=thisdir()/"i1.fxv2",
                                shadertek="tek_imp1",
-                               layer=self.lyr_fwd,
-                               DIM = IMP_DIM,
-                               is_stereo=self.is_stereo )
+                               layer=SGC.layer_fwd,
+                               DIM = IMP_DIM )
     
     imp_mtl = imposter.imp_mtl
     imp_pass = imposter.impdata.imp_pass
@@ -146,37 +101,18 @@ class ImposterApp(object):
     #imposter.impdata.imp_pass.debug_shaderstate = True
     #imposter.impdata.blit_pass.debug_shaderstate = True
 
-    self.scene.lightingmanager.gpuInit(ctx)
+    SG.lightingmanager.gpuInit(ctx)
     
-  ##############################################
-
-  def onUiEvent(self,uievent):
-    res = lev2.ui.HandlerResult()
-    handled = self.uicam.uiEventHandler(uievent)
-    if handled:
-      self.camera.copyFrom( self.uicam.cameradata )
-    return res
-
   ################################################
 
-  def onUpdate(self,updinfo):
-    abstime = updinfo.absolutetime
-    self.time = abstime
-    #########################
-    if (self.extapp==None):
-      if self.is_stereo:
-        x = math.sin(abstime*0.1)
-        z = -math.cos(abstime*0.1)
-        xf_hmd = mtx4.lookAt( vec3(x,0.5,z)*3.0,  # eye
-                              vec3(0,0,0),        # tgt
-                              vec3(0,1,0))        # up
-        self.vrdev.setPoseMatrix("hmd",xf_hmd)
-      self.scene.updateScene(self.cameralut) 
+  def _onUpdate(self,updinfo):
+    self.time = updinfo.absolutetime
+      #self.scene.updateScene(self.cameralut) 
     #########################
 
   ################################################
 
-  def onGpuUpdate(self,ctx):
+  def _onGpuUpdate(self,ctx):
     self.imposter.onGpuUpdate(ctx)
     findex = self.imposter.frame_index
     y = math.sin(findex*0.005)
@@ -187,9 +123,4 @@ class ImposterApp(object):
 ###############################################################################
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description='scenegraph example')
-  parser.add_argument("--stereo", action="store_true", help='enable stereo rendering')
-  ################################################################################
-  args = vars(parser.parse_args())
-  is_stereo = args["stereo"]
-  ImposterApp(is_stereo=is_stereo).ezapp.mainThreadLoop()
+  boilerplate.run(ImposterApp)
