27672446980a80a03695ebbaf0868d20b8b44fb9
Michael T. Mayers <michael@tweakoz.com>
2026-03-09 01:18:35 -0600

    testrunner fixes

--- ork.lev2/pyext/tests/renderer/datasources/marchingcubes2.py
+++ ork.lev2/pyext/tests/renderer/datasources/marchingcubes2.py
@@ -131,10 +131,6 @@ class MCUBES2(ComponentizedApplication):
     # Init lighting
     self.scene.lightingmanager.gpuInit(ctx)
 
-    # Set GPU budget range to 50ms
-    self.profiler.series_gpu_update.setFixedRange(0.0, 50.0)
-    self.profiler.series_draw.setFixedRange(0.0, 50.0)
-    self.profiler.series_present.setFixedRange(0.0, 50.0)
 
   ##############################################
 
